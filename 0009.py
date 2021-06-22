dc4f7200cd364c126aa11744c2f1533c14436fb8
Tommy <tkwang0530@gmail.com>
2021-06-22 23:40:26 +0800

    add note to LC9

--- 0009.py
+++ 0009.py
@@ -9,35 +9,31 @@ Examples:
 """
 
 """
-Solution1: convert to string, and compare it with reversed one
-O(n) time | O(n) space
-"""
-# class Solution:
-#     def isPalindrome(self, x: int) -> bool:
-#         return str(x) == str(x)[::-1]
-
-"""
-Solution2: If we don't want to convert the number to string, then recreate a new number in reverse order.
-O(n) time | O(1) space
-"""
-# class Solution:
-#     def isPalindrome(self, x: int) -> bool:
-#         if x < 0:
-#             return False
-#         temp = x
-#         reversedNum = 0
-#         while temp != 0:
-#             reversedNum = reversedNum * 10 + temp % 10
-#             temp = temp // 10
-#         return x == reversedNum
-"""
-Solution3: recreate a new number in reverse order, and modify x in the same time.
-O(n) time | O(1) space
+Note:
+1. convert to string, and compare it with reversed one: O(n) time | O(n) space
+2. recreate a new number in reverse order: O(n) time | O(1) space
+3. recreate a new number in reverse order (improve): O(n) time | O(1) space
 """
 
 
+
+
+import unittest
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+    def isPalindrome2(self, x: int) -> bool:
+        if x < 0:
+            return False
+        temp = x
+        reversedNum = 0
+        while temp != 0:
+            reversedNum = reversedNum * 10 + temp % 10
+            temp = temp // 10
+        return x == reversedNum
+
+    def isPalindrome3(self, x: int) -> bool:
         if x < 0 or (x > 0 and x % 10 == 0):
             return False
         result = 0
@@ -48,21 +44,22 @@ class Solution:
 
 
 # Unit Tests
-import unittest
+funcs = [Solution().isPalindrome, Solution().isPalindrome2,
+         Solution().isPalindrome3]
 
 
 class TestPalindromeNumber(unittest.TestCase):
     def testPalindromeNumber1(self):
-        func = Solution().isPalindrome
-        self.assertEqual(func(x=121), True)
+        for func in funcs:
+            self.assertEqual(func(x=121), True)
 
     def testPalindromeNumber2(self):
-        func = Solution().isPalindrome
-        self.assertEqual(func(x=-121), False)
+        for func in funcs:
+            self.assertEqual(func(x=-121), False)
 
     def testPalindromeNumber3(self):
-        func = Solution().isPalindrome
-        self.assertEqual(func(x=10), False)
+        for func in funcs:
+            self.assertEqual(func(x=10), False)
 
 
 if __name__ == "__main__":
