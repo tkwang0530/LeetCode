a326e538fefaaacf2646af03e54333aa5f3133b6
Tommy <tkwang0530@gmail.com>
2021-06-23 07:06:27 +0800

    add note to LC24 and change its unittest syntax

--- 0024.py
+++ 0024.py
@@ -10,18 +10,18 @@ Examples:
 Constraints:
 The number of nodes in the list is in the range [0, 100].
 0 <= Node.val <= 100
-
-Follow up:
-Can you solve the problem without modifying the values in the list's nodes?
-(i.e., only nodes themselves may be changed.)
 """
 
 """
 Note:
-To be continued
+1. Iteration (Three Pointers): O(n) time | O(1) space
+2. Recursion: O(n) time | O(n) space
 """
 
 
+
+
+import unittest
 class ListNode:
     def __init__(self, val=0, next=None) -> None:
         self.val = val
@@ -37,49 +37,63 @@ class ListNode:
             self = self.next
         return "->".join(str(num) for num in nums)
 
-
-# O(n) time | O(1) space
-class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        prev = dummy
-        dummy.next = head
-        current = head
-        if not current or not current.next:
-            return current
-        while current is not None and current.next is not None:
-            prev.next = current.next
-            current.next = current.next.next
-            prev.next.next = current
+    @classmethod
+    def fromArray(cls, arr):
+        if arr is None:
+            return None
+        idx = 0
+        length = len(arr)
+        dummy = cls(0)
+        current = dummy
+        while idx < length:
+            current.next = cls(arr[idx])
             current = current.next
-            prev = prev.next.next
+            idx += 1
         return dummy.next
 
 
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        prev = dummy = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            firstNode = head
+            secondNode = head.next
+            prev.next = secondNode
+            firstNode.next = secondNode.next
+            secondNode.next = firstNode
+            prev, head = firstNode, firstNode.next
+        return dummy.next
+
+    def swapPairs2(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        firstNode = head
+        secondNode = head.next
+        firstNode.next = self.swapPairs(secondNode.next)
+        secondNode.next = firstNode
+        return secondNode
+
+
 # Unit Tests
-import unittest
+funcs = [Solution().swapPairs, Solution().swapPairs2]
 
 
 class TestSwapPairs(unittest.TestCase):
     def testSwapPairs1(self):
-        func = Solution().swapPairs
-        head, head.next, head.next.next, head.next.next.next = (
-            ListNode(1),
-            ListNode(2),
-            ListNode(3),
-            ListNode(4),
-        )
-        self.assertEqual(repr(func(head=head)), "2->1->4->3")
+        for func in funcs:
+            head = ListNode.fromArray([1, 2, 3, 4])
+            self.assertEqual(repr(func(head=head)), "2->1->4->3")
 
     def testSwapPairs2(self):
-        func = Solution().swapPairs
-        head = None
-        self.assertEqual(repr(func(head=head)), "None")
+        for func in funcs:
+            head = None
+            self.assertEqual(repr(func(head=head)), "None")
 
     def testSwapPairs3(self):
-        func = Solution().swapPairs
-        head = ListNode(1)
-        self.assertEqual(repr(func(head=head)), "1")
+        for func in funcs:
+            head = ListNode(1)
+            self.assertEqual(repr(func(head=head)), "1")
 
 
 if __name__ == "__main__":
