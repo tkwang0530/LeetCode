829d993e1e3318c606520b7e2c93446a1de69439
Tommy <tkwang0530@gmail.com>
2021-06-29 22:40:52 +0800

    add one more solution to LC151

--- 0151.py
+++ 0151.py
@@ -41,19 +41,49 @@ Follow up: if the string data type is mutable in your language, can you solve it
 """
 Note:
 1. split() + reverse: O(n) time | O(n) space
+2. Two Pointers: O(n) time | O(n) space
+traverse + manual add space + skip space
 """
 
 
 
 
+from typing import List
 import unittest
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(s.split()[::-1])
 
+    def reverseWords2(self, s: str) -> str:
+        words = []
+        left, right = 0, len(s) - 1
+        while left < len(s) and s[left] == " ":
+            left += 1
+        startOfWord = left
+        while right >= 0 and s[right] == " ":
+            right -= 1
+
+        while left <= right:
+            char = s[left]
+            if char == " ":
+                words.append(s[startOfWord:left])
+                while left <= right and s[left] == " ":
+                    left += 1
+                startOfWord = left
+            left += 1
+        words.append(s[startOfWord:right+1])
+        self.reverseList(words)
+        return " ".join(words)
+
+    def reverseList(self, list: List[str]):
+        left, right = 0, len(list) - 1
+        while left < right:
+            list[left], list[right] = list[right], list[left]
+            left, right = left + 1, right - 1
+
 
 # Unit Tests
-funcs = [Solution().reverseWords]
+funcs = [Solution().reverseWords, Solution().reverseWords2]
 
 
 class TestReverseWords(unittest.TestCase):
