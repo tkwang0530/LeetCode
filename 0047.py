cfe1133b203286319dde5b1a4cfb6ee0b5618655
Tommy <tkwang0530@gmail.com>
2021-07-07 00:13:41 +0800

    add another solution to LC47

--- 0047.py
+++ 0047.py
@@ -20,12 +20,15 @@ Constraints:
 
 """
 Note:
-1. Recursion: O(n*n!) time | O(n + n*n!) space
+1. Recursion with concatenation: O(n*n!) time | O(n + n*n!) space
+2. Recursion (swap element + snapshot + set): O(n*n!) time | O(n + n*n!) space
 """
 
+
+
+
+import unittest
 from typing import List
-
-
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -40,28 +43,49 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             curr.append(nums[i])
-            self.helper(nums[0:i] + nums[i + 1 :], curr, answers)
+            self.helper(nums[0:i] + nums[i + 1:], curr, answers)
             curr.pop()
+
+    def permuteUnique2(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+        self.helper2(nums, 0, permutations)
+        return permutations
+
+    def helper2(self, nums, i, permutations):
+        if i == len(nums) - 1:
+            permutations.append(nums[:])
+        else:
+            visited = set()
+            for j in range(i, len(nums)):
+                if nums[j] not in visited:
+                    visited.add(nums[j])
+                    self.swap(nums, i, j)
+                    self.helper2(nums, i + 1, permutations)
+                    self.swap(nums, i, j)
+
+    def swap(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
 
 
 # Unit Tests
-import unittest
+funcs = [Solution().permuteUnique, Solution().permuteUnique2]
 
 
 class TestPermuteUnique(unittest.TestCase):
     def testPermuteUnique1(self):
-        func = Solution().permuteUnique
-        self.assertEqual(
-            func(nums=[1, 1, 2]).sort(),
-            [[1, 1, 2], [1, 2, 1], [2, 1, 1]].sort(),
-        )
+        for func in funcs:
+            self.assertEqual(
+                func(nums=[1, 1, 2]).sort(),
+                [[1, 1, 2], [1, 2, 1], [2, 1, 1]].sort(),
+            )
 
     def testPermuteUnique2(self):
-        func = Solution().permuteUnique
-        self.assertEqual(
-            func(nums=[1, 2, 3]).sort(),
-            [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]].sort(),
-        )
+        for func in funcs:
+            self.assertEqual(
+                func(nums=[1, 2, 3]).sort(),
+                [[1, 2, 3], [1, 3, 2], [2, 1, 3], [
+                    2, 3, 1], [3, 1, 2], [3, 2, 1]].sort(),
+            )
 
 
 if __name__ == "__main__":
