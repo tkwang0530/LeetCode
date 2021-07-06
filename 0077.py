e110d67bbeedf2210104ff4c38a29b01bea8c09e
Tommy <tkwang0530@gmail.com>
2021-07-07 01:32:39 +0800

    add another solution into LC77

--- 0077.py
+++ 0077.py
@@ -26,58 +26,75 @@ Constraints:
 
 """
 Note:
-1. Recursion with backtracking: O(k*n!) time | O(k + k*n!) space
+1. Recursion with backtracking (1): O(k*n!) time | O(k + k*n!) space
+2. Recursion with backtracking (2): O(k*n!) time | O(k + k*n!) space
 """
 
+
+
+
+import unittest
 from typing import List
-
-
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        self.helper(n, 1, k, [], result)
-        return result
+        combinations = []
+        self.helper(n, k, 1, [], combinations)
+        return combinations
 
-    def helper(self, n, i, k, curr_list, result):
-        if len(curr_list) == k:  # backtrack
-            result.append(curr_list.copy())
-            return
-        if i == n + 1:  # can't add n+1 to curr_list
-            return
-        # exclude i
-        self.helper(n, i + 1, k, curr_list, result)
+    def helper(self, n, k, start, combination, combinations):
+        if len(combination) == k:
+            combinations.append(combination[:])
+        else:
+            for i in range(start, n+1):
+                combination.append(i)
+                self.helper(n, k, i + 1, combination, combinations)
+                combination.pop()
 
-        # include i
-        curr_list.append(i)
-        self.helper(n, i + 1, k, curr_list, result)
-        curr_list.pop()
+    def combine2(self, n: int, k: int) -> List[List[int]]:
+        combinations = []
+        self.helper2(n, k, 1, [], combinations)
+        return combinations
+
+    def helper2(self, n, k, start, combination, result):
+        if len(combination) == k:  # backtrack
+            result.append(combination.copy())
+            return
+        if start == n + 1:  # can't add n+1 to combination
+            return
+        # exclude start
+        self.helper2(n, k, start + 1, combination, result)
+
+        # include start
+        combination.append(start)
+        self.helper2(n, k, start + 1, combination, result)
+        combination.pop()
 
 
 # Unit Tests
-import unittest
+funcs = [Solution().combine, Solution().combine2]
 
 
 class TestCombine(unittest.TestCase):
     def testCombine1(self):
-        func = Solution().combine
-        self.assertEqual(
-            func(n=4, k=2).sort(),
-            [
-                [2, 4],
-                [3, 4],
-                [2, 3],
-                [1, 2],
-                [1, 3],
-                [1, 4],
-            ].sort(),
-        )
+        for func in funcs:
+            self.assertEqual(
+                func(n=4, k=2).sort(),
+                [
+                    [2, 4],
+                    [3, 4],
+                    [2, 3],
+                    [1, 2],
+                    [1, 3],
+                    [1, 4],
+                ].sort(),
+            )
 
     def testCombine2(self):
-        func = Solution().combine
-        self.assertEqual(
-            func(n=1, k=1).sort(),
-            [[1]].sort(),
-        )
+        for func in funcs:
+            self.assertEqual(
+                func(n=1, k=1).sort(),
+                [[1]].sort(),
+            )
 
 
 if __name__ == "__main__":
