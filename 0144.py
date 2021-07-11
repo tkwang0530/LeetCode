463cb937a7a105ffc0d12303e9b5bc3af8d242d6
Tommy <tkwang0530@gmail.com>
2021-07-11 21:18:20 +0800

    add Morris traversal solution to LC144

--- 0144.py
+++ 0144.py
@@ -19,6 +19,7 @@ Follow up: Recursive solution is trival, could you do it iteratively?
 Note:
 1. DFS (Iterative Boundary Walk): O(n) time | O(n) space
 use explicit stack (LIFO)
+2. Morris Traversal: O(n) time | O(1) space
 """
 
 
@@ -48,14 +49,47 @@ class Solution:
                 stack.append(treeNode.left)
         return result
 
+    def preorderTraversal2(self, root: TreeNode) -> List[int]:
+        current = root
+        result = []
+        while current:
+            if current.left is None:
+                result.append(current.val)
+                current = current.right
+            else:
+                predecessor = current.left
+                while predecessor.right != current and predecessor.right is not None:
+                    predecessor = predecessor.right
+                if predecessor.right is None:
+                    predecessor.right = current
+                    result.append(current.val)
+                    current = current.left
+                else:
+                    predecessor.right = None
+                    current = current.right
+        return result
+
 # Unit Tests
+
+
+funcs = [Solution().preorderTraversal, Solution().preorderTraversal2]
 
 
 class TestPreorderTraversal(unittest.TestCase):
     def testPreorderTraversal1(self):
-        root = TreeNode(1, None, TreeNode(2, TreeNode(3)))
-        func = Solution().preorderTraversal
-        self.assertEqual(func(root=root), [1, 2, 3])
+        for func in funcs:
+            root = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+            self.assertEqual(func(root=root), [1, 2, 3])
+
+    def testPreorderTraversal2(self):
+        for func in funcs:
+            root = None
+            self.assertEqual(func(root=root), [])
+
+    def testPreorderTraversal3(self):
+        for func in funcs:
+            root = TreeNode(1)
+            self.assertEqual(func(root=root), [1])
 
 
 if __name__ == "__main__":
