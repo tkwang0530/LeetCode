d82a734badcbfe0a0bee85ce8e5d031aaeac9558
Tommy <tkwang0530@gmail.com>
2021-08-02 15:24:07 +0800

    fix typo for LC1539

--- 1539.py
+++ 1539.py
@@ -1,5 +1,5 @@
 """
-1539. K+h Missing Positive Number
+1539. Kth Missing Positive Number
 Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
 Find the kth positive integer that is missing from this array.
 
