fc50241a370ba714e194e5f9749ef880d37d3a8b
Tommy <tkwang0530@gmail.com>
2021-08-12 21:09:27 +0800

    add iterative solution for LC236

--- 0236.py
+++ 0236.py
@@ -34,7 +34,14 @@ p and q will exist in the tree.
 """
 Note:
 1. recursive solution (stop at None): O(n) time | O(n) space
-It's recursive and expands the meaning of the function. If the current (sub)tree contains both p and q, then the function result is their LCA. If only one of them is in that subtree, then the result is that one of them. If neither are in that subtree, the result is null/None/nil.
+3 cases
+If the current (sub)tree contains both p and q, then the function result is their LCA. 
+If only one of them is in that subtree, then the result is that one of them. 
+If neither are in that subtree, the result is null/None/nil.
+2. iterative solution: O(n) time | O(n) space
+(1) traverse the entire tree and build a parent dictionary (node -> node's parent)
+(2) build the p's ancestor set, which contains all the nodes that include p
+(3) using the parent dict and ancestor set, move q upward until q exists in p's ancestor set
 """
 
 
@@ -56,29 +63,54 @@ class Solution:
         lcaFromRight = self.lowestCommonAncestor(root.right, p, q)
         return root if lcaFromLeft and lcaFromRight else lcaFromLeft or lcaFromRight
 
+    def lowestCommonAncestor2(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        stack = [root]
+        parent = { root: None}
+        # build the parent dictionary
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # build p ancestors set (like a path from p to root)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # move q up until q in ancestors set
+        while q not in ancestors:
+            q = parent[q]
+        return q
+
 # Unit Tests
+funcs = [Solution().lowestCommonAncestor, Solution().lowestCommonAncestor2]
 
 
 class TestLowestCommonAncestor(unittest.TestCase):
     def testLowestCommonAncestor1(self):
-        p = TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), TreeNode(4)))
-        q = TreeNode(1, TreeNode(0), TreeNode(8))
-        root = TreeNode(3, p, q)
-        func = Solution().lowestCommonAncestor
-        self.assertEqual(func(root=root, p=p, q=q), root)
+        for func in funcs:
+            p = TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), TreeNode(4)))
+            q = TreeNode(1, TreeNode(0), TreeNode(8))
+            root = TreeNode(3, p, q)
+            self.assertEqual(func(root=root, p=p, q=q), root)
 
     def testLowestCommonAncestor2(self):
-        q = TreeNode(4)
-        expectLca = p = TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), q))
-        root = TreeNode(3, p, TreeNode(1, TreeNode(0), TreeNode(8)))
-        func = Solution().lowestCommonAncestor
-        self.assertEqual(func(root=root, p=p, q=q), expectLca)
+        for func in funcs:
+            q = TreeNode(4)
+            expectLca = p = TreeNode(5, TreeNode(6), TreeNode(2, TreeNode(7), q))
+            root = TreeNode(3, p, TreeNode(1, TreeNode(0), TreeNode(8)))
+            self.assertEqual(func(root=root, p=p, q=q), expectLca)
 
     def testLowestCommonAncestor3(self):
-        q = TreeNode(2)
-        expectLca = root = p = TreeNode(1, q)
-        func = Solution().lowestCommonAncestor
-        self.assertEqual(func(root=root, p=p, q=q), expectLca)
+        for func in funcs:
+            q = TreeNode(2)
+            expectLca = root = p = TreeNode(1, q)
+            self.assertEqual(func(root=root, p=p, q=q), expectLca)
 
 
 if __name__ == "__main__":
