09711bf6793e0231eb2b0802b8929ad040984fb6
Tommy <tkwang0530@gmail.com>
2021-08-20 17:32:57 +0800

    add one solution to LC20

--- 0020.py
+++ 0020.py
@@ -19,8 +19,8 @@ Examples:
 
 """
 Note:
-Using Stack
-O(n) time | O(n) space
+1. Using Stack: O(n) time | O(n) space
+2. Using Stack with Hash Table: O(n) time | O(n) space
 """
 
 
@@ -41,31 +41,46 @@ class Solution:
                     return False
         return len(stack) == 0
 
+    def isValid2(self, s: str) -> bool:
+        dict = {"(": ")", "[": "]", "{": "}"}
+        stack = []
+        for symbol in s:
+            if symbol in dict:
+                stack.append(symbol)
+            else:
+                if len(stack) == 0:
+                    return False
+                key = stack.pop()
+                if dict[key] != symbol:
+                    return False
+        return len(stack) == 0
+
 
 # Unit Tests
 import unittest
+funcs = [Solution().isValid]
 
 
 class TestIsValid(unittest.TestCase):
     def testIsValid1(self):
-        func = Solution().isValid
-        self.assertEqual(func(s="()"), True)
+        for func in funcs:
+            self.assertEqual(func(s="()"), True)
 
     def testIsValid2(self):
-        func = Solution().isValid
-        self.assertEqual(func(s="()[]{}"), True)
+        for func in funcs:
+            self.assertEqual(func(s="()[]{}"), True)
 
     def testIsValid3(self):
-        func = Solution().isValid
-        self.assertEqual(func(s="(]"), False)
+        for func in funcs:
+            self.assertEqual(func(s="(]"), False)
 
     def testIsValid4(self):
-        func = Solution().isValid
-        self.assertEqual(func(s="([)]"), False)
+        for func in funcs:
+            self.assertEqual(func(s="([)]"), False)
 
     def testIsValid5(self):
-        func = Solution().isValid
-        self.assertEqual(func(s="{[]}"), True)
+        for func in funcs:
+            self.assertEqual(func(s="{[]}"), True)
 
 
 if __name__ == "__main__":
