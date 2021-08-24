8a9d12aa6a45b4ad74240bae1e3fc1eea6438b11
Tommy <tkwang0530@gmail.com>
2021-08-24 18:08:46 +0800

    refactor LC1346

--- 1346.py
+++ 1346.py
@@ -47,7 +47,7 @@ class Solution:
     def checkIfExist2(self, arr: List[int]) -> bool:
         nums = set()
         for num in arr:
-            if num * 2 in nums or (num % 2 == 0 and num // 2 in nums):
+            if num * 2 in nums or num / 2 in nums:
                 return True
             nums.add(num)
         return False
