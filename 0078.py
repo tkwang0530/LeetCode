1620c81e9ee7c4fa55042c0e1daa253a181bbd09
Tommy <tkwang0530@gmail.com>
2021-08-29 11:44:31 +0800

    refactor LC78

--- 0078.py
+++ 0078.py
@@ -21,7 +21,8 @@ All the numbers of nums are unique.
 """
 Note:
 1. Iteration: O(n * 2^n) time | O(n * 2^n) space
-2. Recursion: O(n * 2^n) time | O(n * 2^n) space
+2. DFS (append to result during traversal): O(n * 2^n) time | O(n * 2^n) space
+3. DFS (append to result when index == len(nums)): O(n * 2^n) time | O(n * 2^n) space
 """
 
 
@@ -38,35 +39,51 @@ class Solution:
                 subsets.append(currentSubset + [num])
         return subsets
 
-    def subsets2(self, nums: List[int], idx=None) -> List[List[int]]:
-        if idx is None:
-            idx = len(nums) - 1
-        if idx < 0:
-            return [[]]
+    def subsets2(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        self.dfs(nums, result, [], 0)
+        return result
+    
+    def dfs(self, nums: List[int], result: List[List[int]], subset: List[int], index: int):
+        result.append(subset[:])
+        for i in range(index, len(nums)):
+            subset.append(nums[i])
+            self.dfs(nums, result, subset, i + 1)
+            subset.pop()
+
+    def subsets3(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        self.dfs2(nums, result, [], 0)
+        return result
+
+    def dfs2(self, nums: List[int], result: List[List[int]], subset: List[int], index: int):
+        if index == len(nums):
+            result.append(subset[:])
         else:
-            element = nums[idx]
-            subsets = self.subsets2(nums, idx - 1)
-            for i in range(len(subsets)):
-                subset = subsets[i]
-                subsets.append(subset + [element])
-        return subsets
+            self.dfs2(nums, result, subset, index + 1)
+            subset.append(nums[index])
+            self.dfs2(nums, result, subset, index + 1)
+            subset.pop()
+        
 
 
 # Unit Tests
-funcs = [Solution().subsets, Solution().subsets2]
+funcs = [Solution().subsets, Solution().subsets2, Solution().subsets3]
 
 
 class TestSubsets(unittest.TestCase):
     def testSubsets1(self):
         for func in funcs:
             nums = [1, 2, 3]
-            self.assertEqual(func(nums=nums).sort(), [[], [1], [2], [
-                             1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].sort())
+            self.assertEqual(sorted(func(nums=nums)), sorted([[], [1], [2], [
+                             1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]))
 
     def testSubsets2(self):
         for func in funcs:
             nums = [0]
-            self.assertEqual(func(nums=nums).sort(), [[], [0]].sort())
+            self.assertEqual(sorted(func(nums=nums)), sorted([[], [0]]))
 
 
 if __name__ == "__main__":
