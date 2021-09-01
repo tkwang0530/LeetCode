222cc453fa0763bd49b1d004dead3ba7d1e68664
Tommy <tkwang0530@gmail.com>
2021-09-01 15:00:42 +0800

    add LC50

---
+++ 0050.py
@@ -0,0 +1,71 @@
+"""
+50. Pow(x, n)
+Implement pow(x, n), which calculates x raised to the power n (i.e., x^n)
+
+Example1:
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+Example2:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+Example3:
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+"""
+
+"""
+Notes:
+1. Recursion (Divide and conquer): O(logn) time | O(logn) space
+(1) concept
+2^10 = 2^5 * 2^5
+2^5 = 2 * 2^2 * 2^2
+2^2 = 2 * 2
+(2) base case
+x = 0, return 0
+n = 0, return 1
+"""
+class Solution(object):
+    def myPow(self, x: float, n: int) -> float:
+        result = self.myPowHelper(x, abs(n))
+        return result if n >= 0 else 1/result
+
+    def myPowHelper(self, x: float, n: int):
+        if x == 0: return 0
+        if n == 0: return 1
+
+        # could replace with self.myPowHelper(x*x, n // 2) without the next line
+        result = self.myPowHelper(x, n // 2) 
+        result = result * result
+        return x * result if n % 2 else result
+
+
+
+# Unit Tests
+import unittest
+funcs = [Solution().myPow]
+
+class TestMyPow(unittest.TestCase):
+    def testMyPow1(self):
+        for func in funcs:
+            x = 2.00000
+            n = 10
+            self.assertTrue(abs(func(x, n) - 1024) < 1/(10**10))
+
+    def testMyPow2(self):
+        for func in funcs:
+            x = 2.10000
+            n = 3
+            self.assertTrue(abs(func(x, n)-9.26100) < 1/(10**10))
+
+    def testMyPow3(self):
+        for func in funcs:
+            x = 2.00000
+            n = -2
+            self.assertTrue(abs(func(x, n)-0.25) < 1/(10**10))
+
+
+if __name__ == "__main__":
+    unittest.main()
