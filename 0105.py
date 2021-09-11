bef826062f7593bf6763679ce2bfde7e01ed754d
Tommy <tkwang0530@gmail.com>
2021-09-12 00:17:19 +0800

    refactor LC105

--- 0105.py
+++ 0105.py
@@ -22,7 +22,8 @@ Note:
 preorder: self left right
 inorder: left self right
 preprocessing: map a value to its index in inorder list
-map: { 9: 0, 3: 1, 15:2, 20:3, 7:4}
+lookup: { 9: 0, 3: 1, 15:2, 20:3, 7:4}
+2. Iteration with stack: O(n) time | O(n) space
 """
 
 
@@ -39,37 +40,64 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        map = {}
+        lookup = {}
         for i in range(len(inorder)):
-            map[inorder[i]] = i
-        return self.helper(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, map)
-
-    def helper(self, P, startP, endP, I, startI, endI, map) -> TreeNode:
+            lookup[inorder[i]] = i
+        return self.buildTreeHelper(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, lookup)
+    
+    def buildTreeHelper(self, preorder, startP, endP, inorder, startI, endI, lookup) -> TreeNode:
         if startP > endP:
             return None
-        rootNode = TreeNode(P[startP])
-        rootIndex = map[P[startP]]  # get index of rootNode in I
+        root = TreeNode(preorder[startP])
+        rootIndex = lookup[root.val]
         numLeft = rootIndex - startI
-        rootNode.left = self.helper(
-            P, startP + 1, startP + numLeft, I, startI, rootIndex - 1, map)
-        rootNode.right = self.helper(
-            P, startP + numLeft + 1, endP, I, rootIndex + 1, endI, map)
-        return rootNode
+        root.left = self.buildTreeHelper(preorder, startP + 1, startP + numLeft, inorder, startI, rootIndex - 1, lookup)
+        root.right = self.buildTreeHelper(preorder, startP + numLeft + 1, endP, inorder, rootIndex + 1, endI, lookup)
+        return root
+
+    def buildTree2(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        lookup = {}
+        for i in range(len(inorder)):
+            lookup[inorder[i]] = i
+        
+        value = preorder[0]
+        root = TreeNode(value)
+        stack = [root]
+
+        for i in range(1, len(preorder)):
+            value = preorder[i]
+            node = TreeNode(value)
+
+            if lookup[value] < lookup[stack[0].val]:
+                # the new node is on the left of the last node,
+                # so it must be its left child (that's the way inorder works)
+                stack[0].left = node
+            else:
+                # the new node is on the right of the last node,
+                # so it must be the right child of either the last node or one of the last node's ancestors.
+                # pop the stack until we either run out of ancestors
+                # or the node at the top of the stack is to the right of the new node
+                parent = None
+                while len(stack) > 0 and lookup[value] > lookup[stack[0].val]:
+                    parent = stack.pop()
+                parent.right = node
+            stack.append(node)
+        return root
 
 # Unit Tests
-
+funcs = [Solution().buildTree, Solution().buildTree2]
 
 class TestBuildTree(unittest.TestCase):
     def testBuildTree1(self):
-        preorder = [3, 9, 20, 15, 7]
-        inorder = [9, 3, 15, 20, 7]
-        func = Solution().buildTree
-        buildedTreeRoot = func(preorder=preorder, inorder=inorder)
-        self.assertEqual(buildedTreeRoot.val, 3)
-        self.assertEqual(buildedTreeRoot.left.val, 9)
-        self.assertEqual(buildedTreeRoot.right.val, 20)
-        self.assertEqual(buildedTreeRoot.right.left.val, 15)
-        self.assertEqual(buildedTreeRoot.right.right.val, 7)
+        for func in funcs:
+            preorder = [3, 9, 20, 15, 7]
+            inorder = [9, 3, 15, 20, 7]
+            buildedTreeRoot = func(preorder=preorder, inorder=inorder)
+            self.assertEqual(buildedTreeRoot.val, 3)
+            self.assertEqual(buildedTreeRoot.left.val, 9)
+            self.assertEqual(buildedTreeRoot.right.val, 20)
+            self.assertEqual(buildedTreeRoot.right.left.val, 15)
+            self.assertEqual(buildedTreeRoot.right.right.val, 7)
 
 
 if __name__ == "__main__":
