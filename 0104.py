22085204ace45e36c61a52ef1f1d57a1f93f428e
Tommy <tkwang0530@gmail.com>
2021-09-11 22:58:14 +0800

    refactor LC104

--- 0104.py
+++ 0104.py
@@ -17,20 +17,16 @@ return its depth = 3.
 
 """
 Note:
-1. Using Base case 1 (stop at None): O(n) time | O(n) space
-2. Using Base case 2 (stop at leaf): O(n) time | O(n) space
+1. Recursion (stop at None): O(n) time | O(n) space
+2. Recursion (stop at leaf): O(n) time | O(n) space
+3. Iteration (DFS - PreOrder Traversal): O(n) time | O(n) space
 """
 
-
-
-
-import unittest
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
@@ -38,45 +34,51 @@ class Solution:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-    def maxDepth1(self, root: TreeNode) -> int:
+    def maxDepth2(self, root: TreeNode) -> int:
         if root is None:
             return 0
         if root.left is None and root.right is None:
             return 1
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+    def maxDepth3(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        maxDep = 0
+        while len(stack) > 0:
+            node, level = stack.pop()
+            maxDep = max(maxDep, level)
+            if node.left:
+                stack.append((node.left, level+1))
+            if node.right:
+                stack.append((node.right, level+1))
+        return maxDep
 # Unit Tests
-
+import unittest
+funcs = [Solution().maxDepth, Solution().maxDepth2, Solution().maxDepth3]
 
 class TestMaxDepth(unittest.TestCase):
     def testMaxDepth1(self):
-        root = TreeNode(3, TreeNode(9), TreeNode(
-            20, TreeNode(15), TreeNode(7)))
-        func = Solution().maxDepth
-        func2 = Solution().maxDepth1
-        self.assertEqual(func(root=root), 3)
-        self.assertEqual(func2(root=root), 3)
+        for func in funcs:
+            root = TreeNode(3, TreeNode(9), TreeNode(
+                20, TreeNode(15), TreeNode(7)))
+            self.assertEqual(func(root=root), 3)
 
     def testMaxDepth2(self):
-        root = TreeNode(1, None, TreeNode(2))
-        func = Solution().maxDepth
-        func2 = Solution().maxDepth1
-        self.assertEqual(func(root=root), 2)
-        self.assertEqual(func2(root=root), 2)
+        for func in funcs:
+            root = TreeNode(1, None, TreeNode(2))
+            self.assertEqual(func(root=root), 2)
 
     def testMaxDepth3(self):
-        root = None
-        func = Solution().maxDepth
-        func2 = Solution().maxDepth1
-        self.assertEqual(func(root=root), 0)
-        self.assertEqual(func2(root=root), 0)
+        for func in funcs:
+            root = None
+            self.assertEqual(func(root=root), 0)
 
     def testMaxDepth4(self):
-        root = TreeNode(0)
-        func = Solution().maxDepth
-        func2 = Solution().maxDepth1
-        self.assertEqual(func(root=root), 1)
-        self.assertEqual(func2(root=root), 1)
+        for func in funcs:
+            root = TreeNode(0)
+            self.assertEqual(func(root=root), 1)
 
 
 if __name__ == "__main__":
