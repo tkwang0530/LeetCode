267dc628aa9cec5a52c2ab3d08642efe33f95c2c
Tommy <tkwang0530@gmail.com>
2021-09-13 00:17:27 +0800

    refactor unittest syntax for LC5

--- 0005.py
+++ 0005.py
@@ -13,25 +13,22 @@ s consist of only digits and English letters (lower-case and/or upper-case),
 
 """
 Note:
-1. Odd and even Palindrome: O(n^2) time | O(n) space
+1. Odd and even Palindrome: O(n^2) time | O(1+n) space
 """
 
-
-
-
+from typing import Dict
 import unittest
 class Solution(object):
-    def longestPalindrome(self, s: str) -> str:  # s = "ac"
+    def longestPalindrome(self, s: str) -> str:
         currentLongest = [0, 0]  # [leftIdx, rightIdx]
         for i in range(1, len(s)):
             odd = self.getLongestPalindromeFrom(s, i - 1, i + 1)
             even = self.getLongestPalindromeFrom(s, i - 1, i)
             longest = max(odd, even, key=lambda x: x[1] - x[0])
-            currentLongest = max(longest, currentLongest,
-                                 key=lambda x: x[1] - x[0])
+            currentLongest = max(longest, currentLongest, key=lambda x: x[1] - x[0])
         return s[currentLongest[0]: currentLongest[1] + 1]
 
-    def getLongestPalindromeFrom(self, s, leftIdx, rightIdx):  # "ac", 0, 1
+    def getLongestPalindromeFrom(self, s, leftIdx, rightIdx):
         while leftIdx >= 0 and rightIdx < len(s):
             if s[leftIdx] != s[rightIdx]:
                 break
@@ -41,24 +38,24 @@ class Solution(object):
         return [leftIdx + 1, rightIdx - 1]  # [1 , 0]
 
 # Unit Tests
-
+funcs = [Solution().longestPalindrome]
 
 class TestLongestPalindrome(unittest.TestCase):
     def testLongestPalindrome1(self):
-        func = Solution().longestPalindrome
-        self.assertEqual(func(s="babad"), "aba")
+        for func in funcs:
+            self.assertTrue(func(s="babad") in ["aba", "bab", "bad"])
 
     def testLongestPalindrome2(self):
-        func = Solution().longestPalindrome
-        self.assertEqual(func(s="cbbd"), "bb")
+        for func in funcs:
+            self.assertEqual(func(s="cbbd"), "bb")
 
     def testLongestPalindrome3(self):
-        func = Solution().longestPalindrome
-        self.assertEqual(func(s="a"), "a")
+        for func in funcs:
+            self.assertEqual(func(s="a"), "a")
 
     def testLongestPalindrome4(self):
-        func = Solution().longestPalindrome
-        self.assertEqual(func(s="ac"), "c")
+        for func in funcs:
+            self.assertTrue(func(s="ac") in ["a","c"])
 
 
 if __name__ == "__main__":
