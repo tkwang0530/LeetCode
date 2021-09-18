91031921f225a3b7c759f242d4a78bb37b90448c
Tommy <tkwang0530@gmail.com>
2021-09-19 04:27:17 +0800

    refactor LC7

--- 0007.py
+++ 0007.py
@@ -29,10 +29,10 @@ class Solution:
             digit = int(math.fmod(x, 10))     # (python dumb) -1 % 10 = 9
             x = int(x / 10)                             # (python dumb) -1 // 10 = -1
             if (result > MAX // 10 or
-                (result == MAX // 10 and digit >= MAX % 10)):
+                (result == MAX // 10 and digit > MAX % 10)):
                 return 0
             if (result < MIN // 10 or
-                (result == MIN // 10 and digit <= MIN % 10)):
+                (result == MIN // 10 and digit < MIN % 10)):
                 return 0
             result = result * 10 + digit
         return result
