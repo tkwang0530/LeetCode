9843b64c3b420f1c6068564f70e44a957dfa0101
Tommy <tkwang0530@gmail.com>
2021-09-21 16:40:09 +0800

    refactor LC378

--- 0378.py
+++ 0378.py
@@ -28,15 +28,24 @@ All the rows and columns of matrix are guaranteed to be sorted in non-decreasing
 
 """
 Note:
-1. Binary Search: O(nlogn * log(max-min)) time | O(1) space
+1. Convert to 1D array then sort: O(n^2 * logn) time | O(n^2) space
+2. Binary Search: O(nlogn * log(max-min)) time | O(1) space
+3. max Heap with length k: O(n^2 * logk) time | O(k) space
 """
 
 from bisect import bisect_right
 from typing import List
-import unittest
+from heapq import heappush, heappushpop, heappop
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        arr = []
+        for row in range(len(matrix)):
+            arr.extend(matrix[row])
+        arr.sort()
+        return arr[k-1]
+
+    def kthSmallest2(self, matrix: List[List[int]], k: int) -> int:
         left, right = matrix[0][0], matrix[-1][-1]
         while left < right:
             mid = left + (right - left) // 2
@@ -48,11 +57,22 @@ class Solution:
             else:
                 left = mid + 1
         return left
+    
+    def kthSmallest3(self, matrix: List[List[int]], k: int) -> int:
+        heap = []
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if len(heap) < k:
+                    heappush(heap, -matrix[row][col])
+                else:
+                    heappushpop(heap, -matrix[row][col])
+        return -heappop(heap)
 
 
 
 # Unit Tests
-funcs = [Solution().kthSmallest]
+import unittest
+funcs = [Solution().kthSmallest, Solution().kthSmallest2, Solution().kthSmallest3]
 
 class TestKthSmallest(unittest.TestCase):
     def testKthSmallest1(self):
