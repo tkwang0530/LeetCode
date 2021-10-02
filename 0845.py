b467155e9e310a8df157e5307eacfd4d35fcc29e
Tommy <tkwang0530@gmail.com>
2021-10-02 14:13:17 +0800

    refactor LC845

--- 0845.py
+++ 0845.py
@@ -28,39 +28,60 @@ Can you solve it in O(1) space?
 """
 Note:
 1. find peak + expand: O(n) time | O(1) space
+
+2. count up hill length and down hill length for every point: O(n) time | O(1) space
+(1) take one forward pass to count up hill length (to every point).
+(2) take another backward pass to count down hill length (from every point).
+(3) a pass to find max(up[i] + down[i] + 1) where up[i] and down[i] should be positives.
+
+3. count up hill length and down hill length for every point (one pass): O(n) time | O(1) space
+(1) count up and down during the traversal
+(2) reset up and down to zero when A[i-1] == A[i] or down > 0 and A[i-1] < A[i]
 """
 
-
-
-
-import unittest
 from  typing import List
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
+        if len(arr) < 3:
+            return 0
+        peakIdx = 1
         longestPeakLength = 0
-        i = 1
-        while i < len(arr) - 1:
-            isPeak = arr[i-1] < arr[i] and arr[i] > arr[i+1]
+        while peakIdx <= len(arr) - 2:
+            isPeak = arr[peakIdx-1] < arr[peakIdx] and arr[peakIdx] > arr[peakIdx+1]
             if not isPeak:
-                i += 1
+                peakIdx += 1
                 continue
-            leftIdx = i - 2
-            while leftIdx >= 0 and arr[leftIdx] < arr[leftIdx + 1]:
-                leftIdx -= 1
-            rightIdx = i + 2
-            while rightIdx < len(arr) and arr[rightIdx] < arr[rightIdx - 1]:
-                rightIdx += 1
+            
+            left, right = peakIdx - 1, peakIdx + 1
+            while left-1 >= 0 and arr[left-1] < arr[left]:
+                left -= 1
+            while right+1 <= len(arr) - 1 and arr[right+1] < arr[right]:
+                right += 1
+            longestPeakLength = max(longestPeakLength, right - left + 1)
+            peakIdx = right
+        return longestPeakLength
 
-            currentPeakLength = rightIdx - leftIdx - 1
-            longestPeakLength = max(longestPeakLength, currentPeakLength)
-            i = rightIdx
+    def longestMountain2(self, arr: List[int]) -> int:
+        up, down = [0] * len(arr), [0] * len(arr)
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i-1]: up[i] = up[i-1] + 1
+        for i in range(len(arr) - 2, -1, -1):
+            if arr[i] > arr[i+1]: down[i] = down[i+1] + 1
+        return max([u + d + 1 for u, d in zip(up, down) if u and d] or [0])
 
+    def longestMountain3(self, arr: List[int]) -> int:
+        longestPeakLength = up = down = 0
+        for i in range(1, len(arr)):
+            if down and arr[i-1] < arr[i] or arr[i-1] == arr[i]: up = down = 0
+            up += arr[i-1] < arr[i]
+            down += arr[i-1] > arr[i]
+            if up and down:
+                longestPeakLength = max(longestPeakLength, up+down+1)
         return longestPeakLength
 
 # Unit Tests
-
-
-funcs = [Solution().longestMountain]
+import unittest
+funcs = [Solution().longestMountain, Solution().longestMountain2, Solution().longestMountain3]
 
 
 class TestIsMonotonic(unittest.TestCase):
