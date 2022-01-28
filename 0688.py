d30b21b253f1ae9b4b80891003ea0988120084e8
Tommy <tkwang0530@gmail.com>
2022-01-28 09:25:51 +0800

    add LC688

--- 0688.py
+++ 0688.py
@@ -30,6 +30,7 @@ Constraints:
 """
 Note:
 1. Conditional Probability + DFS + HashMap: O(n^2 * k) time | O(n^2 * k) space
+2. Conditional Probability + DP: O(n^2 * k) time | O(n^2) space 
 """
 
 from typing import List, Tuple
@@ -67,20 +68,58 @@ class Solution:
                 return memo[(row, col, moves)]
 
             prob = 0
-            validNextMoves = getValidNextMoves(row, col)
-            for validNextMove in validNextMoves:
-                nextRow, nextCol = validNextMove
+            for nextRow, nextCol in getValidNextMoves(row, col):
                 memo[(nextRow, nextCol, moves)] = dfs(nextRow, nextCol, weight/8, moves+1)
                 prob += memo[(nextRow, nextCol, moves)]
             memo[(row, col, moves)] = prob
             return memo[(row, col, moves)]
         return dfs(row, column, 1, 0)
 
-            
+
+    def knightProbability2(self, n: int, k: int, row: int, column: int) -> float:
+        def isInBoard(row, col) -> bool:
+            return 0 <= row and row <= n-1 and 0 <= col and col <= n-1
+
+        def getNextMoves(row, col) -> List[Tuple[int, int]]:
+            return [
+                (row-1, col+2), 
+                (row-2, col+1), 
+                (row+1, col+2), 
+                (row+2, col+1), 
+                (row+2, col-1), 
+                (row+1, col-2), 
+                (row-1, col-2), 
+                (row-2, col-1)
+            ]
+        
+        # dp0[i][j] is the probability that the knight is on position (i, j)
+        dp0 = [[0]* n for _ in range(n)]
+        dp0[row][column] = 1
+
+        # we maintain the sum of all probabilities in the current dp 2D array
+        result = 1
+        for _ in range(k):
+            # dp1[i][j] is the probability that the knight is on position (i, j) after the current i-th move.
+            dp1 = [[0]* n for _ in range(n)]
+
+            newResult = 0
+            for row in range(n):
+                for col in range(n):
+                    # Try moving the knight from the current position (row, col) to the 8 possible positions.
+                    for nextRow, nextCol in getNextMoves(row, col):
+                        if not isInBoard(nextRow, nextCol):
+                            continue
+
+                        # the new probabilities on position (row, col) equals to the summation of the (old probability / 8.0) from each next position
+                        dp1[row][col] += dp0[nextRow][nextCol] / 8.0
+                        newResult += dp0[row][col] / 8.0
+            dp0 = dp1
+            result = newResult
+        return result
 
 # Unit Tests
 import unittest
-funcs = [Solution().knightProbability]
+funcs = [Solution().knightProbability, Solution().knightProbability2]
 
 class TestKnightProbability(unittest.TestCase):
     def testKnightProbability1(self):
