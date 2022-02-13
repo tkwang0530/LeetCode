0f36615682856fb506329456f43176e93d69ef2f
Tommy <tkwang0530@gmail.com>
2022-02-13 15:41:21 +0800

    refactor LC680

--- 0680.py
+++ 0680.py
@@ -31,21 +31,23 @@ Note:
 import unittest
 class Solution:
     def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(s: str, left: int, right: int) -> bool:
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+        
         left, right = 0, len(s) - 1
         while left < right:
             if s[left] != s[right]:
-                return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
+                return isPalindrome(s, left + 1, right) or isPalindrome(s, left, right - 1)
             left += 1
             right -= 1
         return True
 
-    def isPalindrome(self, s: str, left: int, right: int) -> bool:
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
+    
 
 
 # Unit Tests
