90d30410c4788a72a1444a23c4a766e7c02d1c7c
Tommy <tkwang0530@gmail.com>
2022-05-05 01:18:25 +0800

    add solution into LC242

--- 0242.py
+++ 0242.py
@@ -17,11 +17,12 @@ s and t consist of lowercase English letters.
 
 """
 Note:
-1. Hash Table: O(n) time | O(1) space
-2. Hash Table (rewrite): O(n) time | O(1) space
+1. HashTable: O(n) time | O(1) space
+2. HashTable2 (rewrite): O(n) time | O(1) space
+3. HashTable3: O(n) time | O(1) space
 """
 
-
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -53,23 +54,20 @@ class Solution:
                 return False
         return sum(dict.values()) == 0
 
+    def isAnagram3(self, s: str, t: str) -> bool:
+        return collections.Counter(s) == collections.Counter(t)
 
 # Unit Tests
 import unittest
-
-
+funcs = [Solution().isAnagram, Solution().isAnagram2, Solution().isAnagram3]
 class TestisAnagram(unittest.TestCase):
     def testIsAnagram1(self):
-        func = Solution().isAnagram
-        func2 = Solution().isAnagram2
-        self.assertEqual(func(s="anagram", t="nagaram"), True)
-        self.assertEqual(func2(s="anagram", t="nagaram"), True)
+        for func in funcs:
+            self.assertEqual(func(s="anagram", t="nagaram"), True)
 
     def testIsAnagram2(self):
-        func = Solution().isAnagram
-        func2 = Solution().isAnagram2
-        self.assertEqual(func(s="rat", t="car"), False)
-        self.assertEqual(func2(s="rat", t="car"), False)
+        for func in funcs:
+            self.assertEqual(func(s="rat", t="car"), False)
 
 
 if __name__ == "__main__":
