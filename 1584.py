717871ccb709327cce01d8d5aa9b9f865514b772
Tommy <tkwang0530@gmail.com>
2022-05-14 04:46:35 +0800

    add solution into LC1584

--- 1584.py
+++ 1584.py
@@ -25,9 +25,36 @@ Note:
 1. Prim's Algorithm: O(n^2logn) time | O(n^2) space - where n is the length of points
 (1) Create graph (adjacency list)
 (2) Prim's algorithm with minHeap
+
+2. Kruskal's Algorithm: o(n^2logn) time | O(n^2) space - where n is the length of points
 """
 import collections, heapq
 from typing import List
+
+class UnionFindSet:
+    def __init__(self, n):
+        self._parents = [i for i in range(n+1)]
+        self._ranks = [1 for _ in range(n+1)]
+
+    def find(self, u):
+        while u != self._parents[u]:
+            self._parents[u] = self._parents[self._parents[u]]
+            u = self._parents[u]
+        return u
+
+    def union(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv:
+            return False
+        if self._ranks[pu] < self._ranks[pv]:
+            self._parents[pu] = pv
+        elif self._ranks[pv] < self._ranks[pu]:
+            self._parents[pv] = pu
+        else:
+            self._parents[pu] = pv
+            self._ranks[pv] += 1
+        return True
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
@@ -56,9 +83,30 @@ class Solution:
                 heapq.heappush(minHeap, (neighborCost, neighbor))
         return totalCost
 
+    def minCostConnectPoints2(self, points: List[List[int]]) -> int:
+        n = len(points)
+        minHeap = []
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i+1, n):
+                x2, y2 = points[j]
+                distance = abs(x1-x2) + abs(y1-y2)
+                heapq.heappush(minHeap, (distance, i, j))
+        
+        unionFind = UnionFindSet(n)
+        unionCount = 0
+        totalCost = 0
+        while unionCount < n - 1:
+            distance, i, j = heapq.heappop(minHeap)
+            if unionFind.union(i, j):
+                totalCost += distance
+                unionCount += 1
+        return totalCost
+
+
 # Unit Tests
 import unittest
-funcs = [Solution().minCostConnectPoints]
+funcs = [Solution().minCostConnectPoints, Solution().minCostConnectPoints2]
 
 class TestMinCostConnecPoints(unittest.TestCase):
     def testMinCostConnecPoints1(self):
