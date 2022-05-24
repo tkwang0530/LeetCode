2633a279d0cc4c70de22413c9e19b127274fbe7b
Tommy <tkwang0530@gmail.com>
2022-05-25 04:01:06 +0800

    refactor LC56

--- 0056.py
+++ 0056.py
@@ -1,6 +1,6 @@
 """
 56. Merge Intervals
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input
+Given an array of intervals where intervals[i] = [start_i, end_i], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input
 
 Example1:
 Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
@@ -41,16 +41,12 @@ class TestMerge(unittest.TestCase):
     def testMerge1(self):
         for func in funcs:
             intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-            self.assertEqual(
-                func(intervals=intervals), [[1, 6], [8, 10], [15, 18]]
-            )
+            self.assertEqual(func(intervals=intervals), [[1, 6], [8, 10], [15, 18]])
 
     def testMerge2(self):
         for func in funcs:
             intervals = [[1, 4], [4, 5]]
-            self.assertEqual(
-                func(intervals=intervals), [[1, 5]]
-            )
+            self.assertEqual(func(intervals=intervals), [[1, 5]])
 
 
 if __name__ == "__main__":
