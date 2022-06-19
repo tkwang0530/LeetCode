41dcfb6ff58748ce45b31349344d23860e25aea7
Tommy <tkwang0530@gmail.com>
2022-06-20 00:20:19 +0800

    add solution into LC3

--- 0003.py
+++ 0003.py
@@ -9,15 +9,17 @@ Note that the answer must be a substring, "pwke" is a subsequence and not a subs
 """
 
 """ 
-1. HashTable/Sliding Window: O(n) time | O(1) space
+1. HashTable + Sliding Window: O(n) time | O(1) space
 (1) Use a Hash Table to store the last indies of each characters
 (2) Keep track the valid starting point. When there is a match update the starting point to (current index + 1)
+
+2. HashTable + Sliding Window 2: O(n) time | O(1) space
 """
 
 
 
 
-import unittest
+import collections
 class Solution(object):
     def lengthOfLongestSubstring(self, s: str) -> int:
         lastSeen = {}
@@ -31,11 +33,27 @@ class Solution(object):
             lastSeen[char] = i
         return longest[1] - longest[0] + 1 if len(s) > 0 else 0
 
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        n = len(s)
+        start = 0
+        ans = 0
+        charCounts = collections.defaultdict(int)
+        for end in range(n):
+            # update counter with new end
+            charCounts[s[end]] += 1
+            
+            # move s, until we remove the duplicate character
+            while start < n and charCounts[s[end]] > 1:
+                charCounts[s[start]] -= 1
+                start += 1
+            
+            # after move s, update ans
+            ans = max(ans, end - start + 1)
+        return ans
 
 # Unit Tests
-funcs = [Solution().lengthOfLongestSubstring]
-
-
+import unittest
+funcs = [Solution().lengthOfLongestSubstring, Solution().lengthOfLongestSubstring2]
 class TestLengthOfLongestSubstring(unittest.TestCase):
     def testLengthOfLongestSubstring1(self):
         for func in funcs:
