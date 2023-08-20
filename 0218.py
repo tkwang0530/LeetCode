25cd4013eafd8b1ba3295f6ab1f6362e7a40350b
Tommy <tkwang0530@gmail.com>
2023-08-20 18:43:32 +0800

    add solution into LC218

--- 0218.py
+++ 0218.py
@@ -1,42 +1,18 @@
 """
 218. The Skyline Problem
-A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Given the locations and heights of all the buildings, return the skyline formed by these buildings collectively.
-
-The geometric information of each building is given in the array buildings where buildings[i] = [left_i, right_i, height_i]
-- left_i is the x coordinate of the left edge of the ith building.
-- right_i is the x coordinate of the right edge of the ith building.
-- height_i is the height of the ith building.
-
-You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
-
-The skyline should be represented as a list of "key points" sorted by their x-coordinate in the form [[x1,y1], [x2, y2], ...]. Each key point is the left endpoint of some horizontal segment in the skyline except the last point in the list, which always has a y-coordinate 0 and is used to mark the skyline's termination where the rightmost building ends. Any ground between the leftmost and rightmost buildings should be part of the skyline's contour.
-
-Note: There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [..., [2, 3], [4, 5], [7, 5, [11, 5], [12, 7], ...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [..., [2, 3], [4, 5], [12, 7], ...]
-Input: buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
-Output: [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
-Explanation:
-Figure A shows the buildings of the input.
-Figure B shows the skyline formed by those buildings. The red points in figure B represent the key points in the output list.
-
-Example2:
-Input: buildings = [[0,2,3],[2,5,3]]
-Output: [[0,3],[5,0]]
-
-
-Constraints:
-1 <= buildings.length <= 10^4
-0 <= left_i < right_i <= 2^31 - 1
-1 <= height_i <= 2^31 - 1
-buildings is sorted by left_i in non-decreasing order.
+description: https://leetcode.com/problems/the-skyline-problem/description/
 """
 
 """
 Note:
-1. events and heap: O(nlogn) time | O(n) space
+1. events and heap: O(nlogn) time | O(n) space - where n is the length of array buildings
+2. bst + HashTable: O(nlogn) time | O(n) space - where n is the length of array buildings
 """
 
+import collections
+from sortedcontainers import SortedList
 from typing import List
-from heapq import heappop, heappush
+import heapq
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
         # add start-building events
@@ -56,16 +32,45 @@ class Solution:
             # 2. if it's the start-building event, make the building alive
             # 3. if previous keypoint height != current highest height, edit the result
             while live[0][1] <= pos:
-                heappop(live)
+                heapq.heappop(live)
             if negH:
-                heappush(live, (negH, R))
+                heapq.heappush(live, (negH, R))
             if result[-1][1] != -live[0][0]:
                 result += [ [pos, -live[0][0]] ]
         return result[1:]
 
+
+class Solution2:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        locSet = set()
+        locEvents = collections.defaultdict(list) # <loc>: [diff]
+        for leftLoc, rightLoc, height in buildings:
+            locEvents[leftLoc].append(height)
+            locEvents[rightLoc].append(-1*height)
+            locSet.add(leftLoc)
+            locSet.add(rightLoc)
+        
+        locations = sorted(list(locSet))
+
+        bst = SortedList()
+        maxHeight = 0
+        output = []
+        for loc in locations:
+            for diff in locEvents[loc]:
+                if diff > 0:
+                    bst.add(diff)
+                else:
+                    bst.remove(-1*diff)
+            
+            newMaxHeight = bst[-1] if bst else 0
+            if maxHeight != newMaxHeight:
+                output.append([loc, newMaxHeight])
+            maxHeight = newMaxHeight
+        return output
+
 # Unit Tests
 import unittest
-funcs = [Solution().getSkyline]
+funcs = [Solution().getSkyline, Solution2().getSkyline]
 
 
 class TestGetSkyline(unittest.TestCase):
