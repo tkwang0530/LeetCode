01e5041a81a6a274ed36b7cb08885f965feed306
Tommy <tkwang0530@gmail.com>
2023-09-23 20:52:06 +0800

    refactor LC236

--- 0236.py
+++ 0236.py
@@ -1,44 +1,12 @@
 """
 236. Lowest Common Ancestor of a Binary Tree
-Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
-
-According to the definition of LCA on Wikipedia: "The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself)."
-
-Example1:
-Input: root = [3, 5, 1, 6, 2, 0, 8, null, null, 7 ,4], p = 5, q = 1
-            3
-         /      \
-      5          1
-    /   \        /   \
- 6     2     0      8
-      /   \
-    7      4
-
-Output: 3
-
-Example2:
-Input: root = [3, 5, 1, 6, 2, 0, 8, null, null, 7 ,4], p = 5, q = 4
-Output: 5
-Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
-
-Example3:
-Input: root = [1, 2], p = 1, q = 2
-Output: 1
-
-Constraints:
-All Node.val are unique
-p != q
-p and q will exist in the tree.
+description: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 """
 
 """
 Note:
-1. recursive solution (stop at None): O(n) time | O(n) space
-3 cases
-If the current (sub)tree contains both p and q, then the function result is their LCA. 
-If only one of them is in that subtree, then the result is that one of them. 
-If neither are in that subtree, the result is null/None/nil.
-2. iterative solution: O(n) time | O(n) space
+1. DFS (postOrder Traversal): O(n) time | O(h) space - where n is the number of nodes in the tree and h is the height of the tree
+2. iterative solution: O(n) time | O(n) space - where n is the number of nodes in the tree and h is the height of the tree
 (1) traverse the entire tree and build a parent dictionary (node -> node's parent)
 (2) build the p's ancestor set, which contains all the nodes that include p
 (3) using the parent dict and ancestor set, move q upward until q exists in p's ancestor set
@@ -54,16 +22,22 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        if root in (None, p, q):
-            return root
-        lcaFromLeft = self.lowestCommonAncestor(root.left, p, q)
-        lcaFromRight = self.lowestCommonAncestor(root.right, p, q)
-        return root if lcaFromLeft and lcaFromRight else lcaFromLeft or lcaFromRight
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root):
+            if not root:
+                return None
 
-    def lowestCommonAncestor2(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+            if root in (p, q):
+                return root
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+            return root if left and right else left or right
+
+        return dfs(root)
+class Solution2:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         stack = [root]
         parent = { root: None}
         # build the parent dictionary
@@ -88,7 +62,7 @@ class Solution:
         return q
 
 # Unit Tests
-funcs = [Solution().lowestCommonAncestor, Solution().lowestCommonAncestor2]
+funcs = [Solution().lowestCommonAncestor, Solution2().lowestCommonAncestor]
 
 
 class TestLowestCommonAncestor(unittest.TestCase):
