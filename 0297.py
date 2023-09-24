1c72bd95df0f571867f1db1bf7e2081f242d2a34
Tommy <tkwang0530@gmail.com>
2023-09-24 19:38:37 +0800

    refactor LC297

--- 0297.py
+++ 0297.py
@@ -1,45 +1,21 @@
 """
 297. Serialize and Deserialize Binary Tree
-Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment
-
-Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
-
-Clarification: The input/output format is the same as how Leetcode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
-
-Example1:
-        1
-      /   \
-    2     3 
-         /    \
-       4      5    
-Input: root = [1,2,3,null,null,4,5]
-Output: [1,2,3,null,null,4,5]
-
-Example2:
-Input: root = []
-Output: []
-
-Example3:
-Input: root = [1]
-Output: [1]
-
-Example4:
-Input: root = [1,2]
-Output: [1,2]
-
-Constraints:
-The number of nodes in the tree is in the range [0, 10^4]
--1000 <= Node.val <= 1000
+description: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
 """
 
 """
 Note:
+1. preorder traversal
 For serialize: O(n) time | O(n) space
 (1) Recursive DFS (preorder traversal) to create a list "preorder", if not node, remember to append "None"
 (2) return ",".join(preorder)
 For deserialize: O(n) time | O(n) space
 (1) preorder = data.split(",") and convert to a deque
 (2) Recursively build the tree using the preorder deque
+
+2. BFS (layer order traversal)
+For serialize: O(n) time | O(n) space
+For deserialize: O(n) time | O(n) space
 """
 
 
@@ -83,6 +59,64 @@ class Codec:
         return root
 
 
+class Codec2:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        outputArr = []
+        currentNodes = [root]
+        while currentNodes:
+            nextNodes = []
+            for node in currentNodes:
+                nodeValStr = str(node.val) if node else "null"
+                outputArr.append(nodeValStr)
+                if node:
+                    nextNodes.append(node.left)
+                    nextNodes.append(node.right)
+            currentNodes = nextNodes
+        return ",".join(outputArr)
+
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "":
+            return None
+
+        nodeValStrs = data.split(",")
+        idxNodeMap = {}
+        idxNodeMap[0] = TreeNode(int(nodeValStrs[0]))
+        
+        i = 0
+        j = 1
+        while j < len(nodeValStrs)-1:
+            if j not in idxNodeMap:
+                idxNodeMap[j] = None if nodeValStrs[j] == "null" else TreeNode(int(nodeValStrs[j]))
+            if j+1 not in idxNodeMap:
+                idxNodeMap[j+1] = None if nodeValStrs[j+1] == "null" else TreeNode(int(nodeValStrs[j+1]))
+
+            currentRoot = idxNodeMap[i]
+            left = idxNodeMap[j]
+            right = idxNodeMap[j+1]
+
+            currentRoot.left = left
+            currentRoot.right = right
+            j += 2
+            i += 1
+            while i < j and nodeValStrs[i] == "null":
+                i += 1
+        return idxNodeMap[0]
+
 # TEST ONLY
 def isSameTree(p: TreeNode, q: TreeNode) -> bool:
         if not p and not q:
@@ -91,27 +125,31 @@ def isSameTree(p: TreeNode, q: TreeNode) -> bool:
             return False
         return all((p.val == q.val, isSameTree(p.left, q.left), isSameTree(p.right, q.right)))
 
+classes = [Codec, Codec2]
 class TestCodec(unittest.TestCase):
     def testCodec1(self):
-        tree = TreeNode(1, TreeNode(2), TreeNode(3, TreeNode(4), TreeNode(5)))
-        ser = Codec()
-        deser = Codec()
-        tree2 = deser.deserialize(ser.serialize(tree))
-        self.assertEqual(isSameTree(tree, tree2), True)
+        for myclass in classes:
+            ser = myclass()
+            deser = myclass()
+            tree = TreeNode(1, TreeNode(2), TreeNode(3, TreeNode(4), TreeNode(5)))
+            tree2 = deser.deserialize(ser.serialize(tree))
+            self.assertEqual(isSameTree(tree, tree2), True)
 
     def testCodec2(self):
-        tree = None
-        ser = Codec()
-        deser = Codec()
-        tree2 = deser.deserialize(ser.serialize(tree))
-        self.assertEqual(isSameTree(tree, tree2), True)
+        for myclass in classes:
+            ser = myclass()
+            deser = myclass()
+            tree = None
+            tree2 = deser.deserialize(ser.serialize(tree))
+            self.assertEqual(isSameTree(tree, tree2), True)
 
     def testCodec3(self):
-        tree = TreeNode(1)
-        ser = Codec()
-        deser = Codec()
-        tree2 = deser.deserialize(ser.serialize(tree))
-        self.assertEqual(isSameTree(tree, tree2), True)
+        for myclass in classes:
+            ser = myclass()
+            deser = myclass()
+            tree = TreeNode(1)
+            tree2 = deser.deserialize(ser.serialize(tree))
+            self.assertEqual(isSameTree(tree, tree2), True)
 
     def testCodec4(self):
         tree = TreeNode(1, TreeNode(2))
