f979521441787d8cab2a867883611c44807ed369
Tommy <tkwang0530@gmail.com>
2023-11-25 23:26:16 +0800

    refactor LC1648

--- 1648.py
+++ 1648.py
@@ -1,29 +1,6 @@
 """
 1648. Sell Diminishing-Valued Colored Balls
-You have an inventory of different colored balls, and there is a customer that wants orders balls of any color.
-
-The customer weirdly values the colored balls. Each colored ball's value is the number of balls of that color you currently have in your inventory. For example, if you own 6 yellow balls, the customer would pay 6 for the first yellow ball. After the transaction, there are only 5 yellow balls left, so the next yellow ball is then valued at 5 (i.e., the value of the balls decreases as you sell more to the customer).
-
-You are given an integer array, inventory, where inventory[i] represents the number of balls of the ith color that you initially own. You are also given an integer orders, which represents the total number of balls that the customer wants. You can sell the balls in any order.
-
-Return the maximum total value that you can attain after selling orders colored balls. As the answer may be too large, return it modulo 10^9 + 7.
-
-Example1:
-Input: inventory = [2,5], orders = 4
-Output: 14
-Explanation: Sell the 1st color 1 time (2) and the 2nd color 3 times (5 + 4 + 3).
-The maximum total value is 2 + 5 + 4 + 3 = 14.
-
-Example2:
-Input: inventory = [3,5], orders = 6
-Output: 19
-Explanation: Sell the 1st color 2 times (3 + 2) and the 2nd color 4 times (5 + 4 + 3 + 2).
-The maximum total value is 3 + 2 + 5 + 4 + 3 + 2 = 19.
-
-Constraints:
-1 <= inventory.length <= 10^5
-1 <= inventory[i] <= 10^9
-1 <= orders <= min(sum(inventory[i]), 10^9)
+description: https://leetcode.com/problems/sell-diminishing-valued-colored-balls/description/
 """
 
 """
@@ -35,44 +12,41 @@ import collections
 from typing import List
 class Solution:
     def maxProfit(self, inventory: List[int], orders: int) -> int:
-        MOD = 10**9 + 7
-        inventoryCounter = collections.Counter(inventory)
-        inventoryProp = []
-        for key, count in inventoryCounter.items():
-            inventoryProp.append([key, count])
-        
-        inventoryProp.sort()
-        
-        cost = 0
-        while orders > 0:
-            firstProp = inventoryProp[-1]
-            hasSecond = False
-            secondProp = [0, -1]
-            if len(inventoryProp) >= 2:
-                hasSecond = True
-                secondProp = inventoryProp[-2]
-            
-            
-            diff = firstProp[1] * firstProp[0]
+        countColorCount = collections.defaultdict(int)
+        for color, count in enumerate(inventory):
+            countColorCount[count] += 1
 
-            if hasSecond:
-                diff = (firstProp[0]-secondProp[0]) * firstProp[1]
 
-            diff = min(diff, orders)
-            
-            high = diff // firstProp[1]
-            remain = diff % firstProp[1]
+        countColorCountArr = [[count, colorCount] for count, colorCount in countColorCount.items()]
+        countColorCountArr.sort(key=lambda x: x[0])
 
-            cost = (cost + (firstProp[0]+firstProp[0]-high+1) * high // 2 * firstProp[1]) % MOD
-            cost = (cost + (firstProp[0]-high) * remain) % MOD
+        currentOrders = orders
+        revenue = 0
+        while countColorCountArr and currentOrders > 0:
+            secondCount = countColorCountArr[-2][0] if len(countColorCountArr) >= 2 else 0
+            firstCount = price = countColorCountArr[-1][0]
+            firstColorCount = countColorCountArr[-1][1]
+            countDiff = firstCount - secondCount
 
-            if hasSecond:
-                inventoryProp[-2][1] += inventoryProp[-1][1]
-            
-            orders -= diff
-            inventoryProp.pop()
-  
-        return cost
+            maxConsumeOrders = countDiff * firstColorCount
+            consumeOrders = min(currentOrders, maxConsumeOrders)
+
+            q = consumeOrders // firstColorCount
+            r = consumeOrders % firstColorCount
+
+            """
+            |------------------q--------------------------|
+            price + price-1 + price-2 + price-3 + ... price-q+1
+            """
+            currentRevenue = q * (price+price-q+1) // 2 * firstColorCount + r * (price-q)
+            revenue += currentRevenue
+            currentOrders -= consumeOrders
+
+            if len(countColorCountArr) >= 2:
+                countColorCountArr[-2][1] += firstColorCount
+
+            countColorCountArr.pop()
+        return revenue % (10 ** 9 + 7)
 
 # Unit Tests
 import unittest
