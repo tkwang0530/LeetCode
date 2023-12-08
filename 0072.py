3a6680ab298189c9c02988b0219ae71d7fc85ba8
Tommy <tkwang0530@gmail.com>
2023-12-08 22:35:00 +0800

    added link to problem description for LC72

--- 0072.py
+++ 0072.py
@@ -1,33 +1,6 @@
 """
 72. Edit Distance
-Given two strings word1 and word2, return the minimum number of operations required to convert word1 and word2.
-
-You have the following three operations permitted on a word:
-- Insert a character
-- Delete a character
-- Replace a character
-
-Example1:
-Input: word1 = "horse", word2 = "ros"
-Output: 3
-Explanation: 
-horse -> rorse (replace 'h' with 'r')
-rorse -> rose (remove 'r')
-rose -> ros (remove 'e')
-
-Example2:
-Input: word1 = "intention", word2 = "execution"
-Output: 5
-Explanation: 
-intention -> inention (remove 't')
-inention -> enention (replace 'i' with 'e')
-enention -> exention (replace 'n' with 'x')
-exention -> exection (replace 'n' with 'c')
-exection -> execution (insert 'u')
-
-Constraints:
-0 <= word1.length, word2.length <= 500
-word1 and word2 consist of lowercase English letters.
+description: https://leetcode.com/problems/edit-distance/description/
 """
 
 """
