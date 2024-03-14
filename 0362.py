8c5858335bac851418970fc93a5656bf0f9f7526
Tommy <tkwang0530@gmail.com>
2024-03-14 17:11:31 +0800

    add solution into LC362

--- 0362.py
+++ 0362.py
@@ -42,9 +42,19 @@ Time:
     hit: O(1)
     getHits: O(1) amortized
 Space: O(300)
+
+2. PreSum + Binary Search
+Time:
+    __init__: O(1)
+    hit: O(1)
+    getHits: O(logn)
+Space: O(n)
+This method is better than the first one when the number of hits per second is huge
+and getHits can acceppt a timestamp that is not in chronological order
 """
 
 import collections
+import bisect
 class HitCounter:
 
     # __init__ initializes the object of the hit counter system
@@ -76,10 +86,41 @@ class HitCounter:
             del self.timestampHits[self.queue[0]]
             self.queue.popleft()
 
+class HitCounter2:
+    # Initializes the object of the hit counter system.
+    def __init__(self):
+        self.timestampPreSums = [(0,0)] # the counter include the hit at the timestamp
+
+    # Records a hit that happened at timestamp (in seconds). 
+    # Several hits may happen at the same timestamp.
+    def hit(self, timestamp: int) -> None:
+        if timestamp == self.timestampPreSums[-1][0]:
+            self.timestampPreSums[-1] = (timestamp, self.timestampPreSums[-1][1]+1)
+        else:
+            self.timestampPreSums.append((timestamp, self.timestampPreSums[-1][1]+1))
+
+        
+    # Returns the number of hits in the past 5 minutes from timestamp 
+    # (i.e., the past 300 seconds).
+    def getHits(self, timestamp: int) -> int:
+        left = bisect.bisect_left(self.timestampPreSums, (max(0,timestamp-300+1), -1))
+        if left == len(self.timestampPreSums):
+            return 0
+        
+        leftPreSum = rightPreSum = 0
+        if self.timestampPreSums[left][0] >= timestamp-300+1:
+            leftPreSum = self.timestampPreSums[left-1][1] if left-1 >= 0 else 0
+        else:
+            leftPreSum = self.timestampPreSums[left][1]
+
+        right = bisect.bisect_right(self.timestampPreSums, (timestamp-1, float("inf")))
+        rightPreSum = self.timestampPreSums[right][1] if right < len(self.timestampPreSums) else self.timestampPreSums[-1][1]
+
+        return rightPreSum-leftPreSum
 # Unit Tests
 import unittest
 
-classes = [HitCounter]
+classes = [HitCounter, HitCounter2]
 class TestHitCounter(unittest.TestCase):
     def testHitCounter1(self):
         for myclass in classes: 
