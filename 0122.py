9b8ce7c998d527c9586f452ce7e48be2a0a430ec
Tommy <tkwang0530@gmail.com>
2024-03-27 00:07:44 +0800

    add solution into LC122

--- 0122.py
+++ 0122.py
@@ -36,6 +36,9 @@ which implies T[i-1][k-1][0] = T[i-1][k][0] and T[i-1][k-1][1] = T[i-1][k][1]
 Therefore,
 T[i][k][0] = max(T[i-1][k][0], T[i-1][k][1] + prices[i])
 T[i][k][1] = max(T[i-1][k][1], T[i-1][k-1][0] - prices[i]) = max(T[i-1][k][1], T[i-1][k][0] - prices[i])
+
+2. DP: O(n) time | O(n) space - where n is the length of prices
+ref: https://vocus.cc/article/65fd41d2fd897800015721f3
 """
 
 from typing import List
@@ -50,8 +53,37 @@ class Solution:
             hold = max(hold, oldIdle - price)
         return idle
 
+class Solution2:
+    def maxProfit(self, prices: List[int]) -> int:
+        # state
+        HOLD_STOCK, KEEP_CASH = 0, 1
+
+        dp = {} # <(day, state): maximal profit>
+
+        dp[(-1, HOLD_STOCK)] = -float("inf")
+
+        dp[(-1, KEEP_CASH)] = 0
+
+        for day, price in enumerate(prices):
+            # either today buy stock or
+            # buy stock before today but keep it
+            dp[day, HOLD_STOCK] = max(
+                dp[day-1, KEEP_CASH] - prices[day],
+                dp[day-1, HOLD_STOCK]
+            )
+
+            # either today sell stock or
+            # don't have stock and don't buy stock today
+            dp[day, KEEP_CASH] = max(
+                dp[day-1, HOLD_STOCK] + prices[day],
+                dp[day-1, KEEP_CASH]
+            )
+        
+        lastDay = len(prices) - 1
+        return dp[lastDay, KEEP_CASH]
+
 # Unit Tests
-funcs = [Solution().maxProfit]
+funcs = [Solution().maxProfit, Solution2().maxProfit]
 
 
 class TestMaxProfit(unittest.TestCase):
