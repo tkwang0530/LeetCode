051c572688514f11592ba5bf2a2cee2246ecd11f
Tommy <tkwang0530@gmail.com>
2024-04-03 12:59:45 +0800

    refactor LC22

--- 0022.py
+++ 0022.py
@@ -1,58 +1,41 @@
 """
 22. Generate Parentheses
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
-Example1:
-Input: n = 3
-Output: ["((()))","(()())","(())()","()(())","()()()"]
-
-Example2:
-Input: n = 1
-Output: ["()"]
-
-Constraints:
-1 <= n <= 8
+description: https://leetcode.com/problems/generate-parentheses/description/
 """
 
 """
 Note:
-1. DFS (append to result when index == len(nums)): O(n * 2^(2n)) time | O(n) space
-The time complexity is upper bound
-
-(1) Definition
-left: the number of available open parentheses
-right: the number of available close parentheses
-
-(2) edge case: when left == 0 and right == 0: result.append("".join(chars))
-(3) if left > 0: append "(" to chars, dfs(left+1 ...), and then chars.pop()
-(4) if right > 0 and left < right: append ")" to chars, dfs(right)
+1. backtracking: O(n * 2^(2n)) time | O(2n) space
 """
 
 from typing import List
-
-
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        if n <= 0:
-            return result
-        self.dfs(n, n, [], result)
-        return result
-    
-    def dfs(self, left: int, right: int, chars: List[str], result: List[str]):
-        if left == 0 and right == 0:
-            result.append("".join(chars))
-            return
-        
-        if left > 0:
-            chars.append("(")
-            self.dfs(left - 1, right, chars, result)
-            chars.pop()
-        if right > 0 and left < right:
-            chars.append(")")
-            self.dfs(left, right - 1, chars, result)
-            chars.pop()
+        counter = [n] * 2
+        output = []
+        chars = []
+        def backtrack():
+            if sum(counter) == 0:
+                output.append("".join(chars))
+                return
 
+            # left
+            if counter[0] > 0:
+                counter[0] -= 1
+                chars.append("(")
+                backtrack()
+                chars.pop()
+                counter[0] += 1
+
+            # right
+            if counter[1] > counter[0]:
+                counter[1] -= 1
+                chars.append(")")
+                backtrack()
+                chars.pop()
+                counter[1] += 1
+        backtrack()
+        return output
 
 # Unit Tests
 import unittest
