d6da052890f603efdbcdba73fe3b1cae6e428898
Tommy <tkwang0530@gmail.com>
2024-04-12 16:14:28 +0800

    refactor LC153

--- 0153.py
+++ 0153.py
@@ -1,59 +1,26 @@
 """
 153. Find Minimum in Rotated Sorted Array
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0, 1, 2, 4, 5, 6, 7] might become:
-- [4, 5, 6, 7, 0, 1, 2] if it was rotated 4 times.
-- [0, 1, 2, 4, 5, 6, 7] if it was rotated 7 times.
-
-Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
-
-Given the sorted rotated array nums of unique elements, return the minimum element of this array.
-
-You must write an algorithm that runs in O(logn) time.
-
-Example1:
-Input: nums = [3,4,5,1,2]
-Output: 1
-Explanation: The original array was [1,2,3,4,5] rotated 3 times.
-
-Example2:
-Input: nums = [4,5,6,7,0,1,2]
-Output: 0
-Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
-
-Example3:
-Input: nums = [11,13,15,17]
-Output: 11
-Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
-
-Constraints:
-n == nums.length
-1 <= n <= 5000
--5000 <= nums[i] <= 5000
-All the integers of nums are unique.
-nums is sorted and rotated between 1 and n times.
+description: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 """
 
 """
 Note:
-1. Binary Search: O(logn) time | O(1) space
-if nums[mid] >= nums[left]: move left, otherwise move right 
+1. Binary Search: O(logn) time | O(1) space - where n is the length of array nums
+if nums[mid] >= nums[right]: search right, otherwise search left 
 """
 
 from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left, right = 0, len(nums)
         minVal = nums[0]
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[left] < nums[right-1]:
-                minVal = min(minVal, nums[left])
-                break
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
             minVal = min(minVal, nums[mid])
-            if nums[mid] >= nums[left]:
-                left = mid + 1
+            if nums[mid] >= nums[right]:
+                left = mid+1
             else:
-                right = mid
+                right = mid-1
         return minVal
 
 # Unit Tests
