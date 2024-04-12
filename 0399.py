576aac6f4446c23a4bd82d87ba597442205c5322
Tommy <tkwang0530@gmail.com>
2024-04-12 19:49:19 +0800

    add solution into LC399

--- 0399.py
+++ 0399.py
@@ -1,45 +1,14 @@
 """
 399. Evaluate Division
-You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
-
-You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
-
-Return the answers to all queries. If a single answer cannot be determined, return -1.0 .
-
-Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
-
-Example1:
-Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
-Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
-Explanation: 
-Given: a / b = 2.0, b / c = 3.0
-queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
-return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
-
-Example2:
-Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
-Output: [3.75000,0.40000,5.00000,0.20000]
-
-Example3:
-Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
-Output: [0.50000,2.00000,-1.00000,-1.00000]
-
-Constraints:
-1 <= equations.length <= 20
-equations[i].length == 2
-1 <= Ai.length, Bi.length <= 5
-values.length == equations.length
-0.0 < values[i] <= 20.0
-1 <= queries.length <= 20
-queries[i].length == 2
-1 <= Cj.length, Dj.length <= 5
-Ai, Bi, Ci, Dj consist of lower case English letters and digits.
+description: https://leetcode.com/problems/evaluate-division/description/
 """
 
 """
 Note:
 1. dfs + path compression: O(n^2) time | O(n^2) space
 2. Floyd Warshall: O(n^3) time | O(n^2) space
+3. bfs + hashSet: O(Q*E) time | O(V+E) space - where Q is the length of queries, E is the length of equations, V is the length of variables
+ref: https://www.youtube.com/watch?v=Uei1fwDoyKk
 """
 
 import collections
@@ -96,7 +65,8 @@ class Solution:
                 result.append(-1.0)
         return result
     
-    def calcEquation2(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+class Solution2:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         # graph
         indexTable = {} #<var, the index of a in indexList>
         indexList = []
@@ -152,10 +122,37 @@ class Solution:
                     result.append(-1.0)
         return result
 
+class Solution3:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = collections.defaultdict(list) # <strA, (strB, strA/strB)>
+        for i, (strA, strB) in enumerate(equations):
+            value = values[i]
+            graph[strA].append((strB, value))
+            graph[strB].append((strA, 1/value))
+
+        def bfs(src, target):
+            if src not in graph or target not in graph:
+                return -1
+            
+            queue, visited = collections.deque(), set()
+            queue.append((src, 1))
+            visited.add(src)
+            while queue:
+                node, w = queue.popleft()
+                if node == target:
+                    return w
+                for neighbor, weight in graph[node]:
+                    if neighbor not in visited:
+                        queue.append([neighbor, w * weight])
+                        visited.add(neighbor)
+            return -1
+
+        
+        return [bfs(q[0], q[1]) for q in queries]
 
 # Unit Tests
 import unittest
-funcs = [Solution().calcEquation, Solution().calcEquation2]
+funcs = [Solution().calcEquation, Solution2().calcEquation, Solution3().calcEquation]
 
 class TestCalcEquation(unittest.TestCase):
     def testCalcEquation1(self):
