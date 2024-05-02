6b794aabd4a058011ec8c0e8f32d9eadc3ed5b7d
Tommy <tkwang0530@gmail.com>
2024-05-02 16:27:43 +0800

    refactor LC787

--- 0787.py
+++ 0787.py
@@ -1,59 +1,31 @@
 """
 787. Cheapest Flights Within K Stops
-There are n cities connected by some number of flights. You are given an array flights when flights[i] = [from_i, to_i, price_i] indicates that there is a flight from city from_i to city to_i with cost price_i .
-
-You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
-
-Example1:
-Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
-Output: 200
-Explanation: The graph is shown.
-The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
-
-Example2:
-Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
-Output: 500
-Explanation: The graph is shown.
-The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
-
-Constraints:
-1 <= n <= 100
-0 <= flights.length <= (n * (n - 1) / 2)
-flights[i].length == 3
-0 <= from_i, to_i < n
-from_i != to_i
-1 <= price_i <= 10^4
-There will not be any multiple flights between two cities.
-0 <= src, dst, k < n
-src != dst
+description: https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
 """
 
 """
 Note:
 1. Bellman-Ford algorithm: O(Ek) time | O(n+E) space
+ref: https://www.youtube.com/watch?v=5eIK3zUdYmE
 """
 
 from typing import List
-import collections
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        for source, target, cost in flights:
-            graph[source].append((target, cost))
+        prices = [float("inf")] * n
+        prices[src] = 0
 
-        minCostToCities = [float("inf")] * n
-        minCostToCities[src] = 0
-
-        visitedStop = 0
-        while visitedStop <= k:
-            temp = minCostToCities.copy()
-            for city in range(n):
-                if minCostToCities[city] == float("inf"):
+        for i in range(k+1):
+            tmpPrices = prices.copy()
+            for s, d, p in flights: # s=source, d=destination, p=price
+                if prices[s] == float("inf"):
                     continue
-                for neighbor, cost in graph.get(city, []):
-                    minCostToCities[neighbor] = min(minCostToCities[neighbor], temp[city] + cost)
-            visitedStop += 1
-        return minCostToCities[dst] if minCostToCities[dst] != float("inf") else -1
+
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+
+            prices = tmpPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
             
 
 # Unit Tests
