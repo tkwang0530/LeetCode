e399f7d6492c3e7d941d28a383a0519f650650a9
Tommy <tkwang0530@gmail.com>
2024-05-04 14:39:02 +0800

    add solution into LC392

--- 0392.py
+++ 0392.py
@@ -1,34 +1,20 @@
 """
 392. is Subsequence
-Given two strings s and t, return if s is a subsequence of t, or false otherwise.
-
-A subsequence of a string is a new string that formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
-
-Example1:
-Input: s = "abc", t = "ahbgdc"
-Output: true
-
-Example2:
-Input: s = "rxc", t = "ahbgdc"
-Output: false
-
-Constraints:
-0 <= s.length <= 100
-0 <= t.length <= 10^4
-s and t consist of lowercase English letters.
-
-Follow up: Suppose there are lots of incoming s, say s1, s1, ... sk where k >= 10^9, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+description: https://leetcode.com/problems/is-subsequence/description/
 """
 
 """
 Note:
-1. Two pointers: O(n) time | O(1) space
+1. Two pointers: O(T) time | O(1) space - where T is the length of t
+2. dfs + memo: O(ST) time | O(ST) space - where S is the length of s and T is the length of t
+3. LCS: O(ST) time | O(ST) space - where S is the length of s and T is the length of t
 """
 
 
 
 
 import unittest
+import functools
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         tIdx = 0
@@ -39,10 +25,49 @@ class Solution:
             tIdx += 1
         return sIdx == len(s)
 
+class Solution2:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        S, T = len(s), len(t)
 
+        @functools.lru_cache(None)
+        def dfs(i, j):
+            if i == S:
+                return True
+            elif j == T:
+                return False
+
+            isSub = False
+            # not chose
+            isSub = isSub or dfs(i, j+1)
+
+            # if match, chose
+            isSub = isSub or (s[i]==t[j] and dfs(i+1, j+1))
+            return isSub
+        return dfs(0, 0)
+
+class Solution3:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        charSet = set(s)
+        tChars = [char for char in t if char in charSet]
+        
+        if len(tChars) < len(s):
+            return False
+
+        S, T = len(s), len(tChars)
+        dp = [[0] * (T+1) for _ in range(S+1)]
+
+        for i in range(1, S+1):
+            for j in range(1, T+1):
+                dp[i][j] = max(
+                    dp[i][j], 
+                    dp[i-1][j-1]+(s[i-1]==tChars[j-1]),
+                    dp[i-1][j],
+                    dp[i][j-1]
+                )
+        return dp[-1][-1] == S
 # Unit Tests
 
-funcs = [Solution().isSubsequence]
+funcs = [Solution().isSubsequence, Solution2().isSubsequence, Solution3().isSubsequence]
 
 
 class TestIsSubsequence(unittest.TestCase):
