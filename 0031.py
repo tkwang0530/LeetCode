7e16402d8ea4271ee5e0446d4dfe7713f7674c1b
Tommy <tkwang0530@gmail.com>
2024-05-05 16:14:14 +0800

    refactor LC31

--- 0031.py
+++ 0031.py
@@ -1,30 +1,6 @@
 """
 31. Next Permutation
-Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-
-If such an arrangement is not possible, it must rearrange it as the loweest possible order (i.e., sorted in ascending order).
-
-The replacement must be in place and use only constant extra memory.
-
-Example1:
-Input: nums = [1,2,3]
-Output: [1,3,2]
-
-Example2:
-Input: nums = [3,2,1]
-Output: [1,2,3]
-
-Example3:
-Input: nums = [1,1,5]
-Output: [1,5,1]
-
-Example4:
-Input: nums = [1]
-Output: [1]
-
-Constraints:
-1 <= nums.length <= 100
-0 <= nums[i] <= 100
+description: https://leetcode.com/problems/next-permutation/description/
 """
 
 """
@@ -39,41 +15,29 @@ Notes:
 from typing import List
 
 
-class Solution(object):
+class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        if not nums or len(nums) == 0:
-            return
-
-        small = big = -1
-
-        # find the first number which is less than it's right
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[i+1]:
-                small = i
-                break
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n-2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+        if i >= 0:
+            j = n - 1
+            while j > i and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
         
-        if small == -1:
-            nums.reverse()
-            return
-        
-        # for right to left, find the index where its value is larger than small
-        for i in range(len(nums) - 1, small, -1):
-            if nums[i] > nums[small]:
-                big = i
-                break
-        
-        # exchange small and big
-        nums[small], nums[big] = nums[big], nums[small]
-
-        # reverse the numbers  after "small" index
-        self.reverseFrom(nums, small + 1, len(nums) - 1)
-
-    def reverseFrom(self, nums: List[int], start: int, end: int) -> None:
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
-
+        def reversedFrom(idx):
+            left, right = idx, n - 1
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+            
+        reversedFrom(i+1)
 
 
 # Unit Tests
