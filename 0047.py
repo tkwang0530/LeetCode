febe37f85a231780199c14f7900c3c1d8c7d4820
Tommy <tkwang0530@gmail.com>
2024-05-05 15:38:55 +0800

    refactor LC47

--- 0047.py
+++ 0047.py
@@ -1,21 +1,6 @@
 """
 47. Permutations II
-Given a collection of numbers, nums, that might contain duplicates,return all possible unique permutations in any order.
-
-Example1:
-    Input: [1, 1, 2]
-    Output: [
-        [1, 1, 2],
-        [1, 2, 1],
-        [2, 1, 1]
-    ]
-Example2:
-    Input: [1, 2, 3]
-    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-
-Constraints:
-1 <= nums.length <= 8
--10 <= nums[i] <= 10
+description: https://leetcode.com/problems/permutations-ii/description/
 """
 
 """
