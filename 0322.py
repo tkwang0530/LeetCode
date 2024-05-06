893ddb77a5c15c23a49a9d542c81c45ab598cdfc
Tommy <tkwang0530@gmail.com>
2024-05-06 17:15:04 +0800

    add solutions into LC322

--- 0322.py
+++ 0322.py
@@ -38,34 +38,34 @@ Constraints:
 Note:
 1. BFS + caching (bottom up): O(n * amount) time | O(n* amount) space
 2. DP (bottom up): O(n * amount) time | O(amount) space
+3. dfs+memo: O(n * amount) time | O(n* amount) space
+4. dp: O(n * amount) time | O(amount) space
 """
 
 
-import unittest
-from collections import deque
+import functools, collections, unittest
 from  typing import List
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if not amount:  # Don't need any coin.
+        if amount == 0:
             return 0
 
-        queue = deque([(0, 0)])
-        visited = [True] + [False] * amount
+        queue = collections.deque([(0, 0)]) # (totalCoins, runningAmount)
+        visited = set()
+        visited.add(0)
         while queue:
-            totalCoins, currVal = queue.popleft()
-            totalCoins += 1  # Take a new coin.
+            totalCoins, runningAmount = queue.popleft()
+            if runningAmount == amount:
+                return totalCoins
             for coin in coins:
-                nextVal = currVal + coin
-                if nextVal == amount:  # Find a combination.
-                    return totalCoins
+                nextAmount = runningAmount + coin
+                if nextAmount <= amount and nextAmount not in visited:
+                    visited.add(nextAmount)
+                    queue.append((1+totalCoins, nextAmount))
+        return -1
 
-                if nextVal < amount:  # Could add more coins.
-                    if not visited[nextVal]:
-                        visited[nextVal] = True  # Prevent checking again.
-                        queue.append((totalCoins, nextVal))
-        return -1  # Cannot find any combination.
-
-    def coinChange2(self, coins: List[int], amount: int) -> int:
+class Solution2:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
         for value in range(1, amount + 1):
@@ -74,10 +74,42 @@ class Solution:
                     dp[value] = min(dp[value], 1 + dp[value - coin])
         return dp[amount] if dp[amount] != amount + 1 else - 1
 
+class Solution3:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @functools.lru_cache(None)
+        def dfs(i: int, amount: int) -> int:
+            if amount == 0:
+                return 0
+            
+            if i == len(coins) or amount < 0:
+                return float("inf")
+
+            fewest = float("inf")
+            
+            # skip
+            fewest = min(fewest, dfs(i+1, amount))
+            
+            # use
+            fewest = min(fewest, 1+dfs(i, amount-coins[i]))
+            
+            return fewest
+
+        output = dfs(0, amount)
+        return output if output != float("inf") else -1
+
+class Solution4:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount+1)
+        dp[0] = 0
+        for targetAmount in range(1, amount+1):
+            for coin in coins:
+                if targetAmount-coin >= 0 and dp[targetAmount-coin] != float("inf"):
+                    dp[targetAmount] = min(dp[targetAmount], 1+dp[targetAmount-coin])
+        
+        return dp[-1] if dp[-1] != float("inf") else -1
 
 # Unit Tests
-
-funcs = [Solution().coinChange, Solution().coinChange2]
+funcs = [Solution().coinChange, Solution2().coinChange, Solution3().coinChange, Solution4().coinChange]
 
 
 class TestCoinChange(unittest.TestCase):
