3c70c5b822fa2966a576f4b4be8233f96345de8c
Tommy <tkwang0530@gmail.com>
2024-05-06 17:15:48 +0800

    update description for LC322

--- 0322.py
+++ 0322.py
@@ -1,37 +1,6 @@
 """
 322. Coin Change
-You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-
-Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
-
-You may assume that you have an infinite number of each kind of coin.
-
-Example1:
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-
-Example2:
-Input: coins = [2], amount = 3
-Output: -1
-
-Example3:
-Input: coins = [1], amount = 0
-Output: 0
-
-Example4:
-Input: coins = [1], amount = 1
-Output: 1
-
-Example5:
-Input: coins = [1], amount = 2
-Output: 2
-
-Constraints:
-1 <= coins.length <= 12
-1 <= coins[i] <= 2^31 - 1
-0 <= amount <= 10^4
-
+description: https://leetcode.com/problems/coin-change/description/
 """
 
 """
