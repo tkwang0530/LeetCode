ee3686d85c84009c93c6c46765618da614d3e370
Tommy <tkwang0530@gmail.com>
2024-05-06 17:41:30 +0800

    refactor LC518

--- 0518.py
+++ 0518.py
@@ -1,35 +1,6 @@
 """
 518. Coin Change II
-You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-
-Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
-
-You may assume that you have an infinite number of each kind of coin.
-The answer is guaranteed to fit into a signed 32-bit integer.
-
-Example1:
-Input: amount = 5, coins = [1,2,5]
-Output: 4
-Explanation: there are four ways to make up the amount:
-5=5
-5=2+2+1
-5=2+1+1+1
-5=1+1+1+1+1
-
-Example2:
-Input: amount = 3, coins = [2]
-Output: 0
-Explanation: the amount of 3 cannot be made up just with coins of 2.
-
-Example3:
-Input: amount = 10, coins = [10]
-Output: 1
-
-Constraints:
-1 <= coins.length <= 300
-1 <= coins[i] <= 5000
-All the values of coins are unique.
-0 <= amount <= 5000
+description: https://leetcode.com/problems/coin-change-ii/description/
 """
 
 """
@@ -66,7 +37,8 @@ class Solution:
 
         return dfs(amount, 0)
 
-    def change2(self, amount: int, coins: List[int]) -> int:
+class Solution2:
+    def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount+1)
         dp[0] = 1
 
@@ -77,7 +49,7 @@ class Solution:
 
 
 # Unit Tests
-funcs = [Solution().change, Solution().change2]
+funcs = [Solution().change, Solution2().change]
 
 
 class TestChange(unittest.TestCase):
