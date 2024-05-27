eca119a760ec6c07b9867deb0b37986b25c5a13a
Tommy <tkwang0530@gmail.com>
2024-05-27 09:41:30 +0800

    add solution into LC1608

--- 1608.py
+++ 1608.py
@@ -6,9 +6,11 @@ description: https://leetcode.com/problems/special-array-with-x-elements-greater
 """
 Note:
 1. Counter: O(n) time | O(n) space - where n is the length of array nums
+2. Sort + Binary Search: O(nlogn) time | O(sort) space - where n is the length of array nums
 """
 
 from typing import List
+import bisect
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         counter = [0] * 1001
@@ -22,10 +24,20 @@ class Solution:
                 return i
         
         return -1
+    
+class Solution2:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        for x in range(n+1):
+            count = bisect.bisect_right(nums, x-1)
+            if x == n-count:
+                return x
+        return -1
 
 # Unit Tests
 import unittest
-funcs = [Solution().specialArray]
+funcs = [Solution().specialArray, Solution2().specialArray]
 
 class TestSpecialArray(unittest.TestCase):
     def testSpecialArray1(self):
