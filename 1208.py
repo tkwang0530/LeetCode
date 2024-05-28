608678ca3faccc14171491ab5335a88f0321a7f4
Tommy <tkwang0530@gmail.com>
2024-05-28 22:36:56 +0800

    refactor LC1208

--- 1208.py
+++ 1208.py
@@ -1,37 +1,11 @@
 """
 1208. Get Equal Substrings Within Budget
-You are given two strings s and t of the same length and an integer maxCost.
-
-You want to change s to t. Changing the i-th character of s to i-th character of t costs |s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
-
-Return the maximum length of  a substring of s that can be changed to be the same as the corresponding substring of t with a cost less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
-
-Example1:
-Input: s = "abcd", t = "bcdf", maxCost = 3
-Output: 3
-Explanation: "abc" of s can change to "bcd".
-That costs 3, so the maximum length is 3.
-
-Example2:
-Input: s = "abcd", t = "cdef", maxCost = 3
-Output: 1
-Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
-
-Example3:
-Input: s = "abcd", t = "acde", maxCost = 0
-Output: 1
-Explanation: You cannot make any change, so the maximum length is 1.
-
-Constraints:
-1 <= s.length <= 10^5
-t.length == s.length
-0 <= maxCost <= 10^6
-s and t consist of only lowercase English letters.
+description: https://leetcode.com/problems/get-equal-substrings-within-budget/description/
 """
 
 """
 Note:
-1. Two Pointers: O(n) time | O(1) space - where n is the length of string s
+1. Sliding Window: O(n) time | O(1) space - where n is the length of string s
 """
 
 
@@ -40,18 +14,15 @@ Note:
 import unittest
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        n = len(s)
-        maxLen = 0
         currentCost = 0
         start = 0
-        for end in range(n):
-            sChar, tChar = s[end], t[end]
-            currentCost += abs(ord(sChar) - ord(tChar))
+        maxLen = 0
+        for end in range(len(s)):
+            currentCost += abs(ord(s[end])-ord(t[end]))
             while currentCost > maxCost:
-                sLeftChar, tLeftChar = s[start], t[start]
-                currentCost -= abs(ord(sLeftChar) - ord(tLeftChar))
+                currentCost -= abs(ord(s[start])-ord(t[start]))
                 start += 1
-            maxLen = max(maxLen, end - start + 1)
+            maxLen = max(maxLen, end-start+1)
         return maxLen
 
 
