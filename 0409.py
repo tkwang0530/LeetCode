306a117cc7173181d8e6c643c0f353bab884eabf
Tommy <tkwang0530@gmail.com>
2024-06-04 11:11:07 +0800

    refactor LC409

--- 0409.py
+++ 0409.py
@@ -1,22 +1,6 @@
 """
 409. Longest Palindrome
-Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
-
-Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
-
-Example1:
-Input: s = "abccccdd"
-Output: 7
-Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
-
-Example2:
-Input: s = "a"
-Output: 1
-Explanation: The longest palindrome that can be built is "a", whose length is 1.
-
-Constraints:
-1 <= s.length <= 2000
-s consists of lowercase and/or uppercase English letters only.
+description: https://leetcode.com/problems/longest-palindrome/description/
 """
 
 """ 
@@ -26,18 +10,9 @@ Note:
 import collections
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        charCount = collections.Counter(s)
-        hasOdd = False
-        total = 0
-        for count in charCount.values():
-            if count >= 2:
-                temp = count - 1 if count % 2 == 1 else count
-                total += temp
-                count -= temp
-            if count > 0:
-                hasOdd = True
-            
-        return total+1 if hasOdd else total
+        charCounter = collections.Counter(s)
+        odds = sum([(1 if count % 2 else 0) for count in charCounter.values()])
+        return len(s) - odds + (odds > 0)
 
 # Unit Tests
 import unittest
@@ -45,12 +20,12 @@ funcs = [Solution().longestPalindrome]
 
 class TestLongestPalindrome(unittest.TestCase):
     def testLongestPalindrome1(self):
-        for func in funcs:
-            self.assertEqual(func(s = "abccccdd"), 7)
+        for longestPalindrome in funcs:
+            self.assertEqual(longestPalindrome(s = "abccccdd"), 7)
 
     def testLongestPalindrome2(self):
-        for func in funcs:
-            self.assertEqual(func(s = "a"), 1)
+        for longestPalindrome in funcs:
+            self.assertEqual(longestPalindrome(s = "a"), 1)
 
 if __name__ == "__main__":
     unittest.main()
