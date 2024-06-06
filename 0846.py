3e9700a65e172cde2176f6ec64f3a742dd714162
Tommy <tkwang0530@gmail.com>
2024-06-06 19:29:27 +0800

    add solution into LC846

--- 0846.py
+++ 0846.py
@@ -1,32 +1,15 @@
 """
 846. Hand of Straights
-Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
-
-Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
-
-Example1:
-Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
-Output: true
-Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
-
-Example2:
-Input: hand = [1,2,3,4,5], groupSize = 4
-Output: false
-Explanation: Alice's hand can not be rearranged into groups of 4.
-
-Constraints:
-1 <= hand.length <= 10^4
-0 <= hand[i] <= 10^9
-1 <= groupSize <= hand.length
-
-Note: This question is the same as 1296
+description: https://leetcode.com/problems/hand-of-straights/description/
 """
 
 """ 
 Note:
 1. sort the numberCount: O(nlogn + nm) time | O(n) space - where n is the length of hand and m is the groupSize
+2. SortedList: O(nlogn) time | O(n) space - where n is the length of hand
 """
 
+from sortedcontainers import SortedList
 import collections
 from typing import List
 class Solution(object):
@@ -51,9 +34,25 @@ class Solution(object):
         return True
 
 
+class Solution2:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize > 0:
+            return False
+
+        bst = SortedList()
+        for h in hand:
+            bst.add(h)
+
+        while bst:
+            first = bst[0]
+            for i in range(groupSize):
+                if first+i not in bst:
+                    return False
+                bst.remove(first+i)
+        return True
 # Unit Tests
 import unittest
-funcs = [Solution().isNStraightHand]
+funcs = [Solution().isNStraightHand, Solution2().isNStraightHand]
 
 class TestIsNStraightHand(unittest.TestCase):
     def testIsNStraightHand1(self):
