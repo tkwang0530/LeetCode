16517e0258ea7e11a2f7da9a0c4c88c742bc6c8d
Tommy <tkwang0530@gmail.com>
2024-06-21 23:49:11 +0800

    add solution into LC1052

--- 1052.py
+++ 1052.py
@@ -1,35 +1,12 @@
 """
 1052. Grumpy Bookstore Owner
-There is a bookstore owner that has a store open for n minutes. Every minute, some number of customers enter the store. You are given an integer array customers of length n where customers[i] is the number of the customer that enters the store at the start of the ith minute and all those customers leave after the end of that minute.
-
-On some minutes, the bookstore owner is grumpy. You are given a binary array grumpy where grumpy[i] is 1 if the bookstore owner is grumpy during the ith minute, and is 0 otherwise.
-
-When the bookstore owner is grumpy, the customers of that minute are not satisfied, otherwise, they are satisfied.
-
-The bookstore owner knows a secret technique to keep themselves not grumpy for minutes consecutive minutes, but can only use it once.
-
-Return the maximum number of customers that can be satisfied throughout the day.
-
-Example1:
-Input: customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], minutes = 3
-Output: 16
-Explanation: The bookstore owner keeps themselves not grumpy for the last 3 minutes. 
-The maximum number of customers that can be satisfied = 1 + 1 + 1 + 1 + 7 + 5 = 16.
-
-Example2:
-Input: customers = [1], grumpy = [0], minutes = 1
-Output: 1
-
-Constraints:
-n == customers.length == grumpy.length
-1 <= minutes <= n <= 2 * 10^4
-0 <= customers[i] <= 1000
-grumpy[i] is either 0 or 1.
+description: https://leetcode.com/problems/grumpy-bookstore-owner/description/
 """
 
 """
 Note:
 1. Sliding Window: O(n) time | O(1) space - where n is the length of array customers
+2. Sliding Window (one pass): O(n) time | O(1) space - where n is the length of array customers
 """
 
 
@@ -55,9 +32,29 @@ class Solution:
             maxCount = max(maxCount, currentCount)
         return maxCount
 
+class Solution2:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        start = 0
+        window = 0
+        satisfied = 0
+        maxWindow = 0
+        for end in range(len(customers)):
+            if grumpy[end]:
+                window += customers[end]
+            else:
+                satisfied += customers[end]
+
+            if end - start + 1 > minutes:
+                window -= customers[start] * grumpy[start]
+                start += 1
+            
+            maxWindow = max(maxWindow, window)
+
+        return satisfied + maxWindow
+            
 
 # Unit Tests
-funcs = [Solution().maxSatisfied]
+funcs = [Solution().maxSatisfied, Solution2().maxSatisfied]
 
 
 class TestMaxSatisfied(unittest.TestCase):
