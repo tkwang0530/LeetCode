ed082fe83d208146f0f8c8d9ece838ed3351c7b2
Tommy <tkwang0530@gmail.com>
2024-06-26 22:29:23 +0800

    refactor LC1382

--- 1382.py
+++ 1382.py
@@ -1,27 +1,11 @@
 """
 1382. Balance a Binary Search Tree
-Given a binary search tree, return a balanced binary search tree with the same node values.
-
-A binary search tree is balanced if and only if the depth of the two subtrees of every node never differ by more than 1.
-
-If there is more than one answer, return any of them.
-
-Example1:
-1                              2
-  \                           /    \
-   2                       1       3
-      \                                \
-        3                              4
-          \
-           4
-Input: root = [1,null,2,null,3,null,4,null,null]
-Output: [2,1,3,null,null,null,4]
-Explanation: This is not the only correct answer, [3,1,4,null,2,null,null] is also correct.
+description: https://leetcode.com/problems/balance-a-binary-search-tree/description/
 """
 
 """
 Note:
-1. DFS inorder traverse + recursion: O(n) time | O(n) space
+1. DFS inorder traverse + recursion: O(n) time | O(n+h) space - where n is the number of nodes in the tree and h is the height of the tree
 """
 
 
@@ -40,25 +24,32 @@ class TreeInfo:
 
 class Solution:
     def balanceBST(self, root: TreeNode) -> TreeNode:
-        nums = []
-        self.dfs(root, nums)
-        return self.buildTree(nums, 0, len(nums) - 1)
+        inorders = []
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            inorders.append(node)
+            if node.right:
+                dfs(node.right)
 
-    def dfs(self, root: TreeNode, nums: List[int]):
-        if not root:
-            return
-        self.dfs(root.left, nums)
-        nums.append(root.val)
-        self.dfs(root.right, nums)
-    
-    def buildTree(self, nums: List[int], left: int, right: int):
-        if left > right:
-            return
-        mid = left + (right - left) // 2
-        root = TreeNode(nums[mid])
-        root.left = self.buildTree(nums, left, mid - 1)
-        root.right = self.buildTree(nums, mid + 1, right)
-        return root
+        dfs(root)
+
+        def build(left, right):
+            if left > right:
+                return None
+            elif left == right:
+                root = inorders[left]
+                root.left = root.right = None
+                return root
+            else:
+                mid = left + (right - left) // 2
+                root = inorders[mid]
+                leftSubTree = build(left, mid-1)
+                rightSubTree = build(mid+1, right)
+                root.left, root.right = leftSubTree, rightSubTree
+                return root
+
+        return build(0, len(inorders)-1)
 
     def isBalanced(self, root: TreeNode) -> bool:
         if not root:
@@ -79,6 +70,16 @@ class Solution:
                     stack.append((node.left, False))
         return True
 
+    def inorder(self, root: TreeNode) -> List[int]:
+        inorders = []
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            inorders.append(node.val)
+            if node.right:
+                dfs(node.right)
+        dfs(root)
+        return inorders
 # Unit Tests
 funcs = [Solution().balanceBST]
 
@@ -87,29 +88,23 @@ class TestBalanceBST(unittest.TestCase):
     def testBalanceBST1(self):
         for func in funcs:
             root = TreeNode(1, None, TreeNode(2, None, TreeNode(3, None, TreeNode(4))))
-            inorder = []
-            Solution().dfs(root, inorder)
-            self.assertEqual(inorder, [1, 2, 3, 4])
-            isBalanced = Solution().isBalanced(func(root=root))
-            self.assertEqual(isBalanced, True)
+            newRoot = func(root=root)
+            self.assertEqual(Solution().inorder(root=newRoot), [1, 2, 3, 4])
+            self.assertEqual(Solution().isBalanced(newRoot), True)
 
     def testBalanceBST2(self):
         for func in funcs:
             root = TreeNode(3, TreeNode(2, TreeNode(1)))
-            inorder = []
-            Solution().dfs(root, inorder)
-            self.assertEqual(inorder, [1, 2, 3])
-            isBalanced = Solution().isBalanced(func(root=root))
-            self.assertEqual(isBalanced, True)
+            newRoot = func(root=root)
+            self.assertEqual(Solution().inorder(root=newRoot), [1, 2, 3])
+            self.assertEqual(Solution().isBalanced(newRoot), True)
 
     def testBalanceBST3(self):
         for func in funcs:
             root = TreeNode(3, TreeNode(1, None,TreeNode(2)))
-            inorder = []
-            Solution().dfs(root, inorder)
-            self.assertEqual(inorder, [1, 2, 3])
-            isBalanced = Solution().isBalanced(func(root=root))
-            self.assertEqual(isBalanced, True)
+            newRoot = func(root=root)
+            self.assertEqual(Solution().inorder(root=newRoot), [1, 2, 3])
+            self.assertEqual(Solution().isBalanced(newRoot), True)
 
 
 if __name__ == "__main__":
