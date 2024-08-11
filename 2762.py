4cdd03f590aa347916274ef5a7c9c4aa60c5ba1e
Tommy <tkwang0530@gmail.com>
2024-08-11 10:36:45 +0800

    add LC2762

---
+++ 2762.py
@@ -0,0 +1,48 @@
+"""
+2762. Continuous Subarrays
+description: https://leetcode.com/problems/continuous-subarrays/description/
+"""
+
+"""
+Note:
+1. Backtrack: O(n) time | O(1) space - where n is the length of array nums
+"""
+
+import collections
+from typing import List
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        L = 0
+        counter = collections.defaultdict(int)
+        for R in range(n):
+            counter[nums[R]] += 1
+            while max(counter.keys())-min(counter.keys()) > 2:
+                counter[nums[L]] -= 1
+                if counter[nums[L]] == 0:
+                    del counter[nums[L]]
+                L += 1
+            
+            Loption = R - L + 1
+            count += Loption
+        return count
+
+# Unit Tests
+import unittest
+funcs = [Solution().continuousSubarrays]
+
+class TestContinuousSubarrays(unittest.TestCase):
+    def testContinuousSubarrays1(self):
+        for func in funcs:
+            nums = [5, 4, 2, 4]
+            self.assertEqual(func(nums=nums), 8)
+            
+
+    def testContinuousSubarrays2(self):
+        for func in funcs:
+            nums = [1, 2, 3]
+            self.assertEqual(func(nums=nums), 6)
+
+if __name__ == "__main__":
+    unittest.main()
