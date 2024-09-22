4781fc9e4a1f5d9e6755e42e86c97d5789c16f20
Tommy <tkwang0530@gmail.com>
2024-09-23 01:29:50 +0800

    refactor LC88

--- 0088.py
+++ 0088.py
@@ -1,44 +1,11 @@
 """
 88. Merge Sorted Array
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-
-Example1:
-Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-Output: [1,2,2,3,5,6]
-Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
-
-Example2:
-Input: nums1 = [1], m = 1, nums2 = [], n = 0
-Output: [1]
-Explanation: The arrays we are merging are [1] and [].
-The result of the merge is [1].
-
-Example3:
-Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-Output: [1]
-Explanation: The arrays we are merging are [] and [1].
-The result of the merge is [1].
-Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
-
-Constraints:
-nums1.length == m + n
-nums2.length == n
-0 <= m, n <= 200
-1 <= m + n <= 200
--10^9 <= nums1[i], nums2[j] <= 10^9
-
-Follow up: Can you come up with an algorithm that runs in O(m+n) time ?
+description: https://leetcode.com/problems/merge-sorted-array/description/
 """
 
 """
 Note:
 1. Two Pointers + backward traverse: O(m+n) time | O(1) space
-2. Two Pointers + backward traverse2: O(m+n) time | O(1) space
 """
 
 
@@ -48,41 +15,25 @@ from typing import List
 import unittest
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        idx = m + n-1
-        idx1 = m - 1
-        idx2 = n - 1
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m+n-1
+        idx1 = m-1
+        idx2 = n-1
         while idx1 >= 0 or idx2 >= 0:
-            value1 = nums1[idx1] if idx1 >= 0 else float("-inf")
-            value2 = nums2[idx2] if idx2 >= 0 else float("-inf")
-            if value1 > value2:
-                nums1[idx] = nums1[idx1]
+            val1 = nums1[idx1] if idx1 >= 0 else -float("inf")
+            val2 = nums2[idx2] if idx2 >= 0 else -float("inf")
+            if val1 >= val2:
+                nums1[i] = val1
                 idx1 -= 1
             else:
-                nums1[idx] = nums2[idx2]
+                nums1[i] = val2
                 idx2 -= 1
-            idx -= 1
-
-    def merge2(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        idx1 = m - 1
-        idx2 = n - 1
-        end = len(nums1) - 1
-        while idx1 >= 0 and idx2 >= 0:
-            if nums1[idx1] > nums2[idx2]:
-                nums1[end] = nums1[idx1]
-                idx1 -= 1
-            else:
-                nums1[end] = nums2[idx2]
-                idx2 -= 1
-            end -= 1
-        
-        if idx2 >= 0:
-            nums1[0:idx2+1] = nums2[0:idx2+1]
-        
-
-
+            i -= 1
 
 # Unit Tests
-funcs = [Solution().merge, Solution().merge2]
+funcs = [Solution().merge]
 
 
 class TestMerge(unittest.TestCase):
