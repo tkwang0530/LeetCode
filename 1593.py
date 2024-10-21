d45c51249fbea4c82622c67f6002ca5ba7ddc96c
Tommy <tkwang0530@gmail.com>
2024-10-21 22:19:54 +0800

    add LC1593

--- 1593.py
+++ 1593.py
@@ -1,36 +1,37 @@
 """
 1593. Split a String Into the Max Number of Unique Substrings
-description: https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
+description: https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/ 
 """
 
 """
 Note:
-1. Backtracking: O(2^n * n) time | O(n) space - where n is the length of string s
+1. backtracking + HashSet: O(2^n*n^2) time | O(2^n * n) space - where n is the length of string s
 """
+
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
+        subStrSet = set()
         n = len(s)
-        def dfs(i, uniqueStrSet):
+        maxSplit = 0
+        def backtrack(i):
+            nonlocal maxSplit
             if i == n:
-                return 0
+                maxSplit = max(maxSplit, len(subStrSet))
+                return
 
-            maxUniques = 0
-            for j in range(i+1, n+1):
-                currentStr = s[i:j]
-                if currentStr in uniqueStrSet:
-                    continue
-                uniqueStrSet.add(currentStr)
-                maxUniques = max(maxUniques, 1 + dfs(j, uniqueStrSet))
-                uniqueStrSet.remove(currentStr)
-            return maxUniques
+            for end in range(i+1, n+1):
+                subStr = s[i:end]
+                if subStr not in subStrSet:
+                    subStrSet.add(subStr)
+                    backtrack(end)
+                    subStrSet.remove(subStr)
         
-        uniqueStrSet = set()
-        return dfs(0, uniqueStrSet)
+        backtrack(0)
+        return maxSplit
 
-# Unit Tests
-import unittest
 funcs = [Solution().maxUniqueSplit]
 
+import unittest
 class TestMaxUniqueSplit(unittest.TestCase):
     def testMaxUniqueSplit1(self):
         for func in funcs:
