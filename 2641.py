f61eec076d2613bb83e47d2fd36c75d1c97b0a3f
Tommy <tkwang0530@gmail.com>
2024-10-23 19:31:23 +0800

    add LC2641

--- 2641.py
+++ 2641.py
@@ -1,4 +1,3 @@
-
 """
 2641. Cousins in Binary Tree II
 description: https://leetcode.com/problems/cousins-in-binary-tree-ii/description/
@@ -6,74 +5,74 @@ description: https://leetcode.com/problems/cousins-in-binary-tree-ii/description
 
 """
 Note:
-1. BFS (layer order traversal): O(n) time | O(n) space - where n is the number of nodes in the tree
+1. BFS (Layer Order Traversal): O(n) time | O(n) space - where n is the number of nodes in the tree
 """
-import collections
-from typing import Optional
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+import collections
+from typing import Optional
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        currentNodes = [(root, None)] # (node, parent)
-        while currentNodes:
-            nextNodes = []
-            total = 0
-            parentSumMap = collections.defaultdict(int)
-            for node, parent in currentNodes:
-                total += node.val
-                parentSumMap[parent] += node.val
+        currentLayer = [(root, None)]
+        while currentLayer:
+            nextLayer = []
+            currentSum = 0
+            parentSums = collections.defaultdict(int)
+            for node, p in currentLayer:
+                parentSums[p] += node.val
+                currentSum += node.val
                 if node.left:
-                    nextNodes.append((node.left, node))
+                    nextLayer.append((node.left, node))
                 if node.right:
-                    nextNodes.append((node.right, node))
+                    nextLayer.append((node.right, node))
+
+            for node, p in currentLayer:
+                node.val = currentSum - parentSums[p]
             
-            for node, parent in currentNodes:
-                node.val = total - parentSumMap[parent]
-            
-            currentNodes = nextNodes
+            currentLayer = nextLayer
         return root
 
-# Unit Tests
 import unittest
 funcs = [Solution().replaceValueInTree]
-
 class TestReplaceValueInTree(unittest.TestCase):
     def testReplaceValueInTree1(self):
-        for replaceValueInTree in funcs:
+        for func in funcs:
             node5 = TreeNode(5)
-            node4 = TreeNode(4)
-            node9 = TreeNode(9)
-            node1 = TreeNode(1)
-            node10 = TreeNode(10)
+            node4, node9 = TreeNode(4), TreeNode(9)
+            node1, node10 = TreeNode(1), TreeNode(10)
             node7 = TreeNode(7)
             node5.left, node5.right = node4, node9
             node4.left, node4.right = node1, node10
             node9.right = node7
+
             root = node5
-            self.assertEqual(replaceValueInTree(root=root), root)
+            func(root=root)
             self.assertEqual(node5.val, 0)
             self.assertEqual(node4.val, 0)
             self.assertEqual(node9.val, 0)
+
             self.assertEqual(node1.val, 7)
+
             self.assertEqual(node10.val, 7)
             self.assertEqual(node7.val, 11)
 
     def testReplaceValueInTree2(self):
-        for replaceValueInTree in funcs:
+        for func in funcs:
             node3 = TreeNode(3)
-            node2 = TreeNode(2)
-            node1 = TreeNode(1)
+            node1, node2 = TreeNode(1), TreeNode(2)
             node3.left, node3.right = node1, node2
+            
             root = node3
-            self.assertEqual(replaceValueInTree(root=root), root)
+            func(root=root)
+            self.assertEqual(node3.val, 0)
             self.assertEqual(node1.val, 0)
             self.assertEqual(node2.val, 0)
-            self.assertEqual(node3.val, 0)
 
 if __name__ == "__main__":
     unittest.main()
