5510d46c9e1c477877149b2f56fbb7174b7fbe58
Tommy <tkwang0530@gmail.com>
2024-10-27 23:08:06 +0800

    refactor LC70

--- 0070.py
+++ 0070.py
@@ -1,22 +1,6 @@
 """
 70. Climbing Stairs
-You are climbing a staircase. It takes n steps to reach the top.
-
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-Example1:
-Input: n = 2
-Output: 2
-Explanation: There are two ways to climb to the top.
-1. 1 step + 1 step
-2. 2 steps
-
-Example2:
-Input: n = 3
-Output: 3
-
-Constraints:
-1 <= n <= 45
+description: https://leetcode.com/problems/climbing-stairs/description/
 """
 
 """
