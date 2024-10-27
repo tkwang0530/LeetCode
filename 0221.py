d9365079117f61fae6d9b90a64763db52aff9902
Tommy <tkwang0530@gmail.com>
2024-10-27 21:56:55 +0800

    add solution into LC221

--- 0221.py
+++ 0221.py
@@ -6,9 +6,10 @@ description: https://leetcode.com/problems/maximal-square/description/
 """
 Note:
 1. dp: O(mn) time | O(1) space
+2. dp (1D): O(mn) time | O(n) space
 """
 
-import unittest
+import unittest, collections
 from typing import List
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
@@ -27,9 +28,30 @@ class Solution:
                 maxArea = max(maxArea, matrix[row][col] ** 2)
         return maxArea
 
+class Solution2:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        dp = collections.defaultdict(int)
+
+        maxArea = 0
+        for row in range(rows):
+            dp1 = collections.defaultdict(int)
+            for col in range(cols):
+                if matrix[row][col] == "1":
+                    dp1[col] = 1 + min(
+                        dp[col],
+                        dp[col-1],
+                        dp1[col-1]
+                    )
+                    maxArea = max(maxArea, dp1[col] ** 2)
+            dp = dp1
+
+        return maxArea
+
 # Unit Tests
 import unittest
-funcs = [Solution().maximalSquare]
+funcs = [Solution().maximalSquare, Solution2().maximalSquare]
 class TestMaximalSquare(unittest.TestCase):
     def testMaximalSquare1(self):
         for func in funcs:
