7eb7757f7613d4dc91b053640ffc7b04972841f8
Tommy <tkwang0530@gmail.com>
2024-10-27 22:54:45 +0800

    add LC14

--- 0014.py
+++ 0014.py
@@ -1,60 +1,35 @@
 """
 14. Longest Common Prefix
-Write a function to find the longest common prefix string amongst an array of strings
-If there is no common prefix, return an empty string "".
-
-Example1:
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
-
-Example2:
-Input: strs = ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
-
-Constraints:
-1 <= strs.length <= 200
-0 <= strs[i].length <= 200
-strs[i] consists of only lower-case English letters.
+description: https://leetcode.com/problems/longest-common-prefix/description/
 """
 
 """
 Note:
-1. one pass (while + for): O(n) time | O(n) space - where n is the length of common prefix
-2. one pass (for + for): O(n) time | O(1) space - where n is the length of common prefix
-find the shortest string first
+1. Brute-Force: O(mn) time | O(m) space - where m is the length of array strs and m is the minimum length from the strings 
 """
 
+import unittest
 from typing import List
-class Solution(object):
+
+class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        prefixEndIndex = 0
-        while True:
-            if len(strs[0]) == prefixEndIndex:
-                return strs[0]
-            char = strs[0][prefixEndIndex]
-            for str in strs:
-                if len(str) == prefixEndIndex or char != str[prefixEndIndex]:
-                    return str[:prefixEndIndex]
-            prefixEndIndex += 1
-
-    def longestCommonPrefix2(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        shortest = min(strs, key=len)
-        for i, char in enumerate(shortest):
-            for str in strs:
-                if str[i] != char:
-                    return shortest[:i]
-        return shortest
-
+        minLen = min([len(s) for s in strs])
+        common = []
+        i = 0
+        while i < minLen:
+            allSame = True
+            for j in range(1, len(strs)):
+                s = strs[j]
+                if s[i] != strs[j-1][i]:
+                    return "".join(common)
+            
+            common.append(strs[0][i])
+            i += 1
+        return "".join(common)
 
 # Unit Tests
 import unittest
-funcs = [Solution().longestCommonPrefix, Solution().longestCommonPrefix2]
-
+funcs = [Solution().longestCommonPrefix]
 class TestLongestCommonPrefix(unittest.TestCase):
     def testLongestCommonPrefix1(self):
         for func in funcs:
