4ea23ecdd44c3bf49838f8fd933516e8f2f0ee2f
Tommy <tkwang0530@gmail.com>
2024-10-27 23:02:46 +0800

    refactor LC151

--- 0151.py
+++ 0151.py
@@ -1,41 +1,6 @@
 """
 151. Reverse Words in a String
-Given an input string s, reverse the order of the words.
-
-A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
-
-Return a string of the words in reverse order concatenated by a single space.
-
-Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
-
-Example1:
-Input: s = "the sky is blue"
-Output: "blue is sky the"
-
-Example2:
-Input: s = "  hello world  "
-Output: "world hello"
-Explanation: Your reversed string should not contain leading or trailing spaces.
-
-Example3:
-Input: s = "a good   example"
-Output: "example good a"
-Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
-
-Example4:
-Input: s = "  Bob    Loves  Alice   "
-Output: "Alice Loves Bob"
-
-Example5:
-Input: s = "Alice does not even like bob"
-Output: "bob like even not does Alice"
-
-Constraints:
-1 <= s.length <= 10^4
-s contains English letters (upper-case and lower-case), digits, and spaces ' '.
-There is at least one word in s.
-
-Follow up: if the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+description: https://leetcode.com/problems/reverse-words-in-a-string/description/
 """
 
 """
