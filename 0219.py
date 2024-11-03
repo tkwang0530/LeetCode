110c3823e70cb32b264b2f6f51bca0d5ecd482d7
Tommy <tkwang0530@gmail.com>
2024-11-04 03:24:05 +0800

    refactor LC219

--- 0219.py
+++ 0219.py
@@ -1,23 +1,6 @@
 """
 219. Contains Duplicate II
-Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k
-
-Example1:
-Input: nums = [1,2,3,1], k = 3
-Output: true
-
-Example2:
-Input: nums = [1,0,1,1], k = 1
-Output: true
-
-Example3:
-Input: nums = [1,2,3,1,2,3], k = 2
-Output: false
-
-Constraints:
-1 <= nums.length <= 10^5
--10^9 <= nums[i] <= 10^9
-0 <= k <= 10^5
+description: https://leetcode.com/problems/contains-duplicate-ii/description/
 """
 
 """ 
