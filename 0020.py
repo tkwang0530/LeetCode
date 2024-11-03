c9c4116edc1c5b85d82fccd7923e31adc3d20852
Tommy <tkwang0530@gmail.com>
2024-11-04 03:39:32 +0800

    refactor LC20

--- 0020.py
+++ 0020.py
@@ -1,20 +1,6 @@
 """
 20. Valid Parentheses
-Given a string s containing just the characters '(', ')', '[', ']', '{', '}', determine if the input string is valid.
-An input string is valid if:
-1. Open brackets must be closed by the same type of brackets.
-2. Open brackets must be closed in the correct order.
-
-
-Examples:
-    Input: s = "()"
-    Output: true
-
-    Input: s = "()[]{}"
-    Output: true
-
-    Input: s = "(]"
-    Output: false
+description: https://leetcode.com/problems/valid-parentheses/description/
 """
 
 """
