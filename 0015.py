e32d0883dfb37c2b8cccff333e6d3629c46859a0
Tommy <tkwang0530@gmail.com>
2024-11-04 01:10:08 +0800

    refactor LC15

--- 0015.py
+++ 0015.py
@@ -1,25 +1,6 @@
 """
 15. 3Sum
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
-
-Notice that the solution set must not contain duplicate triplets.
-
-Example1:
-Input: nums = [-1,0,1,2,-1,-4]
-Output: [[-1,-1,2],[-1,0,1]]
-
-Example2:
-Input: nums = []
-Output: []
-
-Example3:
-Input: nums = [0]
-Output: []
-
-Constraints:
-0 <= nums.length <= 3000
--10^5 <= nums[i] <= 10^5
-
+description: https://leetcode.com/problems/3sum/description/
 """
 
 """
